32ccc638cd25c458799d63007273f8722dcdded4
christian-calabrese <christian.calabrese@pagopa.it>
2024-06-13 10:00:14 +0200

    feat: added app-prod-cd github environment

--- infra/repository/outputs.tf
+++ infra/repository/outputs.tf
@@ -1,6 +1,7 @@
 output "environment_prod" {
   value = {
-    ci = github_repository_environment.github_repository_environment_prod_ci.environment
-    cd = github_repository_environment.github_repository_environment_prod_cd.environment
+    ci     = github_repository_environment.github_repository_environment_prod_ci.environment
+    cd     = github_repository_environment.github_repository_environment_prod_cd.environment
+    app_cd = github_repository_environment.github_repository_environment_app_prod_cd.environment
   }
 }
