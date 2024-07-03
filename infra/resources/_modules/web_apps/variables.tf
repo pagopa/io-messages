b87d00fb399ee8a0ff18e9f5b0315011120f3bcc
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2024-07-03 18:17:12 +0200

    setup io-p-itn-msgs-notif-func

---
+++ infra/resources/_modules/web_apps/variables.tf
@@ -0,0 +1,42 @@
+variable "tags" {
+  type        = map(any)
+  description = "Resources tags"
+}
+
+variable "environment" {
+  type = object({
+    prefix    = string
+    env_short = string
+    location  = string
+    domain    = string
+  })
+
+  description = "Values which are used to generate resource names and location short names. They are all mandatory except for domain, which should not be used only in the case of a resource used by multiple domains."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group to deploy resources to"
+}
+
+variable "private_dns_zone_resource_group_name" {
+  type        = string
+  description = "Resource group name of the private DNS zone to use for private endpoints"
+}
+
+variable "virtual_network" {
+  type = object({
+    name                = string
+    resource_group_name = string
+  })
+}
+
+variable "subnet_pep_id" {
+  type = string
+}
+
+variable "subnet_cidrs" {
+  type = object({
+    notif_func = string
+  })
+}
