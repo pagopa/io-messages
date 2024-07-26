2270199ff45cfe82375a0f31e0540b3819f0aebb
Garma <giovanniarmaleo00@gmail.com>
2024-07-26 11:15:37 +0200

    feat: improve checks when calling redis for set setEx and del

--- apps/citizen-func/utils/redis_storage.ts
+++ apps/citizen-func/utils/redis_storage.ts
@@ -3,60 +3,69 @@ import * as O from "fp-ts/lib/Option";
 import * as TE from "fp-ts/lib/TaskEither";
 import * as redis from "redis";
 
+const isSetResponseOk = (response: string): boolean => response === "OK";
+const isDelResponseOk = (response: number): boolean => response >= 0;
+
+const falsyResponseToErrorAsync = (error: Error) => (
+  response: TE.TaskEither<Error, boolean>
+): TE.TaskEither<Error, true> =>
+  pipe(
+    response,
+    TE.chain(value => (value ? TE.right(value) : TE.left(error)))
+  );
+
 export const setWithExpirationTask = (
   redisClientTask: TE.TaskEither<Error, redis.RedisClientType>,
   key: string,
   value: string,
   expirationInSeconds: number,
-  errorMsg?: string
+  errorMsg: string = "Error setting key value pair on redis"
 ): TE.TaskEither<Error, boolean> =>
   pipe(
     redisClientTask,
     TE.chain(client =>
       TE.tryCatch(
         () => client.setEx(key, expirationInSeconds, value),
-        () =>
-          new Error(
-            errorMsg ? errorMsg : "Error setting key value pair on redis"
-          )
+        () => new Error(errorMsg)
       )
     ),
-    TE.map(constTrue)
+    TE.map(isSetResponseOk),
+    falsyResponseToErrorAsync(new Error(errorMsg))
   );
 
 export const setTask = (
   redisClientTask: TE.TaskEither<Error, redis.RedisClientType>,
   key: string,
   value: string,
-  errorMsg?: string
+  errorMsg: string = "Error setting key value pair on redis"
 ): TE.TaskEither<Error, boolean> =>
   pipe(
     redisClientTask,
     TE.chain(client =>
       TE.tryCatch(
         () => client.set(key, value),
-        () =>
-          new Error(
-            errorMsg ? errorMsg : "Error setting key value pair on redis"
-          )
+        () => new Error(errorMsg)
       )
     ),
-    TE.map(constTrue)
+    TE.map(isSetResponseOk),
+    falsyResponseToErrorAsync(new Error(errorMsg))
   );
 
 export const deleteTask = (
   redisClientTask: TE.TaskEither<Error, redis.RedisClientType>,
-  key: string
+  key: string,
+  errorMsg: string = "Error deleting key value pair on redis"
 ): TE.TaskEither<Error, boolean> =>
   pipe(
     redisClientTask,
     TE.chain(client =>
       TE.tryCatch(
         () => client.del(key),
-        () => new Error("Error deleting key value pair on redis")
+        () => new Error(errorMsg)
       )
     ),
-    TE.map(constTrue)
+    TE.map(isDelResponseOk),
+    falsyResponseToErrorAsync(new Error(errorMsg))
   );
 
 export const getTask = (
