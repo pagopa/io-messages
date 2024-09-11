f827b1f8a11ad740613d6c87adbf51d08a16666a
Garma <64153597+Garma00@users.noreply.github.com>
2024-09-11 11:57:50 +0200

    Update infra/resources/_modules/monitoring/action_group.tf
    Co-authored-by: Luca Cavallaro <luca.cavallaro@pagopa.it>

--- infra/resources/_modules/monitoring/action_group.tf
+++ infra/resources/_modules/monitoring/action_group.tf
@@ -1,4 +1,4 @@
-resource "azurerm_monitor_action_group" "io_com_error_action_group" {
+resource "azurerm_monitor_action_group" "io_com_error" {
   name                = "io-p-com-error-ag-01"
   resource_group_name = var.resource_group_name
   short_name          = "iocom-error"
