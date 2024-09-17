8ea3e659df44610796890179518f171cfcc8c412
Andrea Grillo <andrea.grillo@pagopa.it>
2024-09-17 16:53:11 +0200

    import notification hubs

--- infra/resources/prod/westeurope.tf
+++ infra/resources/prod/westeurope.tf
@@ -1,3 +1,10 @@
+resource "azurerm_resource_group" "notifications" {
+  name     = "${local.project_legacy}-rg-notifications"
+  location = "westeurope"
+
+  tags = local.tags
+}
+
 module "storage_api_weu" {
   source = "../_modules/storage_accounts"
 
@@ -9,6 +16,22 @@ module "storage_api_weu" {
   resource_group_name = data.azurerm_resource_group.internal_rg.name
 
   error_action_group_id = data.azurerm_monitor_action_group.io_com_action_group.id
+
+  tags = local.tags
+}
+
+module "notification_hubs_weu" {
+  source = "../_modules/notification_hubs"
+
+  project                    = local.project_legacy
+  legacy_resource_group_name = azurerm_resource_group.notifications.name
+  resource_group_name        = data.azurerm_resource_group.weu_common.name
+  location                   = azurerm_resource_group.notifications.location
+  location_short             = "weu"
+
+  key_vault_common_id = data.azurerm_key_vault.weu_common.id
+
+  action_group_id = data.azurerm_monitor_action_group.io_com_action_group.id
 
   tags = local.tags
 }
