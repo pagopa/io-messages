2b2a48456c8a065a1881a8ac35f349d498795c22
acuiuli <andrea.cuiuli@gmail.com>
2024-09-30 16:41:03 +0200

    removed unused env var

--- apps/etl-func/src/adapters/env.ts
+++ apps/etl-func/src/adapters/env.ts
@@ -1,7 +1,3 @@
 import { z } from "zod";
 
-export const envSchema = z
-  .object({
-    APPLICATIONINSIGHTS_CONNECTION_STRING: z.string().min(1),
-  })
-  .and(z.record(z.string(), z.string()));
+export const envSchema = z.object({}).and(z.record(z.string(), z.string()));
