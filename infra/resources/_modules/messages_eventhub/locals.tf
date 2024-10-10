c062336e463d2a7b7066768cd78bb61df83ceffd
Luca Mancosu <luca.mancosu@it.ey.com>
2024-10-10 17:54:28 +0200

    changing azurerm version. changing resource group for the messages eventhub

--- infra/resources/_modules/messages_eventhub/locals.tf
+++ infra/resources/_modules/messages_eventhub/locals.tf
@@ -2,7 +2,7 @@ locals {
   eventhub_name              = "${var.prefix}-${var.env_short}-${var.location}-${var.domain}-messages-evh-01"
   eventhub_partition_count   = 32
   eventhub_message_retention = 7
-  resource_group_name        = "io-p-evt-rg"
+  resource_group_name        = "io-p-itn-com-rg-01"
   eventhub_auth_messages_keys = [{
     name   = "io-messages"
     listen = false
