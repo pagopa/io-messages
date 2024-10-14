bbd4f35138052ed42c38d1bbf5c817f55170d6f5
Luca Mancosu <luca.mancosu@it.ey.com>
2024-10-14 15:41:59 +0200

    changing locals

--- infra/resources/_modules/messages_eventhub/locals.tf
+++ infra/resources/_modules/messages_eventhub/locals.tf
@@ -2,7 +2,7 @@ locals {
   eventhub_name              = "${var.prefix}-${var.env_short}-${var.location}-${var.domain}-messages-evh-01"
   eventhub_partition_count   = 32
   eventhub_message_retention = 7
-  resource_group_name        = "io-p-evt-rg"
+  resource_group_name        = "io-p-itn-com-rg-01"
   eventhub_auth_messages_keys = [{
     name   = "io-messages"
     listen = false
@@ -16,5 +16,5 @@ locals {
       manage = false
   }]
 
-  subnet_pep_id = "io-p-vnet-common/io-p-eventhub-snet"
+  subnet_pep_id = "io-p-itn-common-vnet-01/io-p-itn-pep-snet-01"
 }
