b72b2e4718612238774ea5c18ddeb81c03349cb3
Garma <giovanniarmaleo00@gmail.com>
2024-10-14 16:33:38 +0200

    refactor: clean unused interfaces

--- apps/etl-func/src/domain/interfaces/message-content-repository.ts
+++ apps/etl-func/src/domain/interfaces/message-content-repository.ts
@@ -6,33 +6,18 @@ import {
   MessageContent,
   MessageMetadata,
 } from "../entities/message.js";
-import { ContentNotFoundError } from "./errors.js";
 
-export type GetMessageByMetadataReturnType =
-  | ContentNotFoundError
-  | Message
-  //TODO: remove this
-  | z.ZodError;
+export type GetMessageByMetadataReturnType = Message | z.ZodError | RestError;
 
 export type GetMessageContentByIdReturnType =
   | MessageContent
-  | RestError
-  | z.ZodError;
+  | z.ZodError
+  | RestError;
 
 export interface MessageContentRepository {
   getMessageByMetadata: (
     metadata: MessageMetadata,
   ) => Promise<GetMessageByMetadataReturnType>;
-  /**
-   * Retrieve the content of the message identified by the messageId parameter.
-   *
-   * @param messageId {string}
-   * @throws {ContentNotFoundError} There is no content for this message.
-   * @throws {SyntaxError} The content of the message is not a valid JSON.
-   * @throws {z.ZodError} The content of the message does not satisfy the MessageContent shape.
-   * @throws {Error} Something happened trying to retrieve the content of the message.
-   * @returns {MessageContent} the content of the message.
-   */
   getMessageContentById: (
     messageId: string,
   ) => Promise<GetMessageContentByIdReturnType>;
