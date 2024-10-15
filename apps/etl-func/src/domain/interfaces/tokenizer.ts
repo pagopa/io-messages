9fdd57006e124dac44b6288aa53e9747f1e605b8
acuiuli <andrea.cuiuli@gmail.com>
2024-10-15 11:56:50 +0200

    small fix

--- apps/etl-func/src/domain/interfaces/tokenizer.ts
+++ apps/etl-func/src/domain/interfaces/tokenizer.ts
@@ -8,11 +8,11 @@ const messageTokenizedSchema = z.object({
   fiscalCode: z.string().uuid().min(1),
 });
 
-export type messageTokenized = z.infer<typeof messageTokenizedSchema>;
+export type MessageTokenized = z.infer<typeof messageTokenizedSchema>;
 
 export const tokenize =
   (message: { fiscalCode: string }) =>
-  async (client: TokenizerClient): Promise<messageTokenized> => {
+  async (client: TokenizerClient): Promise<MessageTokenized> => {
     const tokenizedFiscalCode = await client.tokenize(message.fiscalCode);
     return { ...message, fiscalCode: tokenizedFiscalCode };
   };
