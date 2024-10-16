5c35bce5b27fb497b703d49240428a4c9a2de77e
Luca Mancosu <luca.mancosu@it.ey.com>
2024-10-16 11:34:30 +0200

    fixing subnet pep id

--- infra/resources/prod/main.tf
+++ infra/resources/prod/main.tf
@@ -231,6 +231,6 @@ module "messages_eventhub" {
 
   resource_group_name                  = data.azurerm_subnet.pep.resource_group_name
   private_dns_zone_resource_group_name = data.azurerm_resource_group.evt-rg.name
-  subnet_pep_id                        = data.azurerm_subnet.pep.name
+  subnet_pep_id                        = data.azurerm_subnet.pep.id
   tags                                 = local.tags
 }
