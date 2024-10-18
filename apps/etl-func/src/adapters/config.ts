34a4738d6a4239ae5f15f79c0685544985919b3a
Garma <giovanniarmaleo00@gmail.com>
2024-10-18 13:44:39 +0200

    feat: add healthcheck for storage in healthcheck func

--- apps/etl-func/src/adapters/config.ts
+++ apps/etl-func/src/adapters/config.ts
@@ -26,33 +26,3 @@ export const configFromEnvironment = envSchema
     }),
   )
   .pipe(configSchema);
-
-// TODO(IOCOM-1786): move this function in "io-messages-common"
-export async function loadConfigFromEnvironment(
-  onSuccess: (config: Config) => Promise<void>,
-) {
-  try {
-    const config = configFromEnvironment.parse(process.env);
-    await onSuccess(config);
-  } catch (err) {
-    if (err instanceof ZodError) {
-      err.issues.forEach((issue) => {
-        logger.error({ issue }, "Error parsing environment variable");
-      });
-    } else if (err instanceof Error) {
-      logger.error(
-        {
-          err,
-        },
-        err.message,
-      );
-    } else {
-      logger.error(
-        {
-          err,
-        },
-        "Unable to start the application due to an unexpected error",
-      );
-    }
-  }
-}
--- apps/etl-func/src/func.ts
+++ apps/etl-func/src/func.ts
@@ -1,10 +1,29 @@
 import { app } from "@azure/functions";
+import { BlobServiceClient } from "@azure/storage-blob";
+import { AzureCliCredential } from "@azure/identity";
+import { loadConfigFromEnvironment } from "io-messages-common/adapters/config";
+import { Config, configFromEnvironment } from "./adapters/config.js";
 
-app.http("Health", {
-  authLevel: "anonymous",
-  handler: () => ({
-    body: "it works!",
-  }),
-  methods: ["GET"],
-  route: "health",
-});
+const main = async (config: Config) => {
+  const azureCredentials = new AzureCliCredential();
+  const blobServiceCLient = new BlobServiceClient(
+    config.messageContentStorage.accountUri,
+    azureCredentials,
+  );
+  app.http("Health", {
+    authLevel: "anonymous",
+    handler: async () => {
+      // check for storage availability or throw
+      console.log("Checking for account info");
+      await blobServiceCLient.getProperties();
+      return {
+        body: "it works!",
+      };
+    },
+    methods: ["GET"],
+    route: "health",
+  });
+
+};
+
+await loadConfigFromEnvironment(main, configFromEnvironment);
