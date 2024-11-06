6719bdeb7dc35a268dc05090fe756a4a483f5aa6
Luca Mancosu <luca.mancosu@it.ey.com>
2024-11-06 16:01:56 +0100

    retrieving connections uri from data

--- infra/resources/_modules/web_apps/locals.tf
+++ infra/resources/_modules/web_apps/locals.tf
@@ -4,11 +4,9 @@ locals {
   project        = "${var.environment.prefix}-${var.environment.env_short}-${local.location_short}"
 
   app_settings = {
-    NODE_ENVIRONMENT = "production",
-    FUNCTIONS_WORKER_RUNTIME : "node",
-    MESSAGE_CONTENT_STORAGE_URI : "https://testgiovannist.blob.core.windows.net/",
-    MESSAGE_CONTENT_CONTAINER_NAME : "messages",
-    MESSAGE_EVENTHUB_CONNECTION_URI : "https://io-d-evh-cdc.servicebus.windows.net",
-    MESSAGE_EVENTHUB_NAME : "message-events-test"
+    NODE_ENVIRONMENT                = "production",
+    FUNCTIONS_WORKER_RUNTIME        = "node",
+    MESSAGE_CONTENT_STORAGE_URI     = var.etl_app_settings.message_content_storage_uri
+    MESSAGE_EVENTHUB_CONNECTION_URI = var.etl_app_settings.message_eventhub_connection_uri,
   }
 }
--- infra/resources/_modules/web_apps/variables.tf
+++ infra/resources/_modules/web_apps/variables.tf
@@ -73,3 +73,10 @@ variable "action_group_id" {
   type        = string
   description = "The ID of the action group"
 }
+
+variable "etl_app_settings" {
+  type = object({
+    message_content_storage_uri : string,
+    message_eventhub_connection_uri : string,
+  })
+}
