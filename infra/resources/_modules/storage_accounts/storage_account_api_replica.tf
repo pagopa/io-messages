0b6ca242046020916d4348ebee2889c1b281ca38
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-07 10:48:41 +0100

    CES-469-migrate-iopstapireplica private endpoint added

--- infra/resources/_modules/storage_accounts/storage_account_api_replica.tf
+++ infra/resources/_modules/storage_accounts/storage_account_api_replica.tf
@@ -34,9 +34,12 @@ module "storage_api_replica" {
 module "azure_storage_account" {
   source = "github.com/pagopa/dx//infra/modules/azure_storage_account?ref=main"
 
-  environment         = var.environment
+  environment         = local.itn_environment
   resource_group_name = var.resource_group_name
   access_tier         = "Hot"
+
+  subnet_pep_id                        = data.azurerm_subnet.subnet_pep_itn.id
+  private_dns_zone_resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
 
   subservices_enabled = {
     blob  = true
