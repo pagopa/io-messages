2da9a8cb3f917f71571c56715c9d5f9738584bc8
Luca Mancosu <luca.mancosu@it.ey.com>
2024-11-07 14:52:53 +0100

    adding a regex for checking the eventhub connection uri env var

--- apps/etl-func/src/adapters/eventhub/config.ts
+++ apps/etl-func/src/adapters/eventhub/config.ts
@@ -1,7 +1,16 @@
 import { z } from "zod";
 
+const eventhubConnectionUriPattern =
+  /^https:\/\/[a-zA-Z0-9-]+\.servicebus\.windows\.net$/;
+
 export const eventhubConfigSchema = z.object({
-  connectionUri: z.string(),
+  connectionUri: z
+    .string()
+    .url()
+    .refine((uri) => eventhubConnectionUriPattern.test(uri), {
+      message:
+        "Invalid eventhub connectionUri. It must be in the 'https://<NamespaceName>.servicebus.windows.net' format",
+    }),
   eventHubName: z.string().min(1),
 });
 
