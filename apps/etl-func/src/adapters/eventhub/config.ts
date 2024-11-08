290f630f597e200a87e3203f7608194e0b4da808
Luca Mancosu <luca.mancosu@it.ey.com>
2024-11-08 14:54:34 +0100

    fixing regex

--- apps/etl-func/src/adapters/eventhub/config.ts
+++ apps/etl-func/src/adapters/eventhub/config.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
 
 export const eventhubConfigSchema = z.object({
-  connectionUri: z.string(),
-  // .regex(/^https:\/\/[a-zA-Z0-9-]+\.servicebus\.windows\.net$/),
+  connectionUri: z.string().regex(/^[a-zA-Z0-9-]+\.servicebus\.windows\.net$/),
   eventHubName: z.string().min(1),
 });
 
