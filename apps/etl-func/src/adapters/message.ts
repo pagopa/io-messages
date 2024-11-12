0fee38613afe3f9479ed70b0c5e4695c65b24dc9
Garma <giovanniarmaleo00@gmail.com>
2024-11-12 15:39:37 +0100

    refactor: add messageEventSchema.parse in getMessageEventFromMessage

--- apps/etl-func/src/adapters/message.ts
+++ apps/etl-func/src/adapters/message.ts
@@ -4,6 +4,7 @@ import {
   MessageEvent,
   MessageMetadata,
   MessageRepository,
+  messageEventSchema,
 } from "@/domain/message.js";
 import { Logger } from "pino";
 
@@ -18,30 +19,31 @@ export const getMessageEventFromMessage = ({
   contentType,
   id,
   metadata,
-}: Message): MessageEvent => ({
-  content_type: contentType,
-  feature_level_type: metadata.featureLevelType,
-  has_attachments: content.third_party_data?.has_attachments ?? false,
-  has_precondition:
-    content.third_party_data?.has_precondition === "ALWAYS" ||
-    content.third_party_data?.has_precondition === "ONCE",
-  has_remote_content: content.third_party_data?.has_remote_content ?? false,
-  id: id,
-  is_pending: metadata.isPending ?? false,
-  op: "CREATE",
-  payment_data_amount: content.payment_data?.amount ?? null,
-  payment_data_invalid_after_due_date:
-    content.payment_data?.invalid_after_due_date ?? null,
-  payment_data_notice_number: content.payment_data?.notice_number ?? null,
-  payment_data_payee_fiscal_code:
-    content.payment_data?.payee?.fiscal_code ?? null,
-  require_secure_channels: content.require_secure_channels,
-  schema_version: 1,
-  sender_service_id: metadata.senderServiceId,
-  sender_user_id: metadata.senderUserId,
-  subject: content.subject,
-  timestamp: new Date(metadata.createdAt).getTime(),
-});
+}: Message): MessageEvent =>
+  messageEventSchema.parse({
+    content_type: contentType,
+    feature_level_type: metadata.featureLevelType,
+    has_attachments: content.third_party_data?.has_attachments,
+    has_precondition:
+      content.third_party_data?.has_precondition === "ALWAYS" ||
+      content.third_party_data?.has_precondition === "ONCE",
+    has_remote_content: content.third_party_data?.has_remote_content,
+    id: id,
+    is_pending: metadata.isPending,
+    op: "CREATE",
+    payment_data_amount: content.payment_data?.amount ?? null,
+    payment_data_invalid_after_due_date:
+      content.payment_data?.invalid_after_due_date ?? null,
+    payment_data_notice_number: content.payment_data?.notice_number ?? null,
+    payment_data_payee_fiscal_code:
+      content.payment_data?.payee?.fiscal_code ?? null,
+    require_secure_channels: content.require_secure_channels,
+    schema_version: 1,
+    sender_service_id: metadata.senderServiceId,
+    sender_user_id: metadata.senderUserId,
+    subject: content.subject,
+    timestamp: new Date(metadata.createdAt).getTime(),
+  });
 
 export interface MessageContentProvider {
   getByMessageId(messageId: string): Promise<MessageContent>;
