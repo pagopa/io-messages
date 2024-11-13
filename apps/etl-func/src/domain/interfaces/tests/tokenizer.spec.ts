005521c39aa672dbba1100b2a3b89a47cc03a17d
acuiuli <andrea.cuiuli@gmail.com>
2024-11-13 09:41:33 +0100

    test fix

--- apps/etl-func/src/domain/interfaces/tests/tokenizer.spec.ts
+++ apps/etl-func/src/domain/interfaces/tests/tokenizer.spec.ts
@@ -1,36 +1,9 @@
-import { MessageMetadata } from "@/domain/message.js";
 import { describe, expect, it, vi } from "vitest";
 
-import {
-  MessageMetadataWithoutPII,
-  TokenizerClient,
-  maskSensitiveInfo,
-} from "../tokenizer.js";
+import { TokenizerClient, maskSensitiveInfo } from "../tokenizer.js";
 
 const aFiscalCode = "AAADPZ44E08F367A";
 const aTokenizedFiscalCode = "3f5a5e37-63a0-423c-a108-94b535e03f91";
-
-const aMessageMetadata: MessageMetadata = {
-  createdAt: "12-02-2024",
-  featureLevelType: "ADVANCED",
-  fiscalCode: aFiscalCode,
-  id: "01ARZ3NDEKTSV4RRFFQ69G5FAV",
-  indexedId: "01ARZ3NDEKTSV4RRFFQ69G5FAV",
-  senderServiceId: "aSenderServiceId",
-  senderUserId: "aSenderUserId",
-  timeToLiveSeconds: 0,
-};
-
-const aMessageMetadataWithFiscalCodeTokenized: MessageMetadataWithoutPII = {
-  createdAt: "12-02-2024",
-  featureLevelType: "ADVANCED",
-  id: "01ARZ3NDEKTSV4RRFFQ69G5FAV",
-  indexedId: "01ARZ3NDEKTSV4RRFFQ69G5FAV",
-  recipientId: aTokenizedFiscalCode,
-  senderServiceId: "aSenderServiceId",
-  senderUserId: "aSenderUserId",
-  timeToLiveSeconds: 0,
-};
 
 const tokenizerClient: TokenizerClient = {
   tokenize: async () => aTokenizedFiscalCode,
@@ -38,15 +11,15 @@ const tokenizerClient: TokenizerClient = {
 
 describe("tokenizeMessage", () => {
   it("should return a message with the fiscal code tokenized if the tokenizer client api is successful", async () => {
-    const run = await maskSensitiveInfo(aMessageMetadata)(tokenizerClient);
-    expect(run).toEqual(aMessageMetadataWithFiscalCodeTokenized);
+    const run = await maskSensitiveInfo(aFiscalCode)(tokenizerClient);
+    expect(run).toEqual(aTokenizedFiscalCode);
   });
 
   it("should return an error if the tokenizer api call return an error", async () => {
     vi.spyOn(tokenizerClient, "tokenize").mockRejectedValue(
       new Error("Error during tokenizer api call"),
     );
-    const run = maskSensitiveInfo(aMessageMetadata)(tokenizerClient);
+    const run = maskSensitiveInfo(aFiscalCode)(tokenizerClient);
     await expect(run).rejects.toThrow("Error during tokenizer api call");
   });
 });
