ce857110cf0ef96de05c8799f169ddec448007f1
Garma <giovanniarmaleo00@gmail.com>
2024-11-20 10:03:09 +0100

    chore: add fiscalCode type

--- apps/etl-func/src/domain/message-status.ts
+++ apps/etl-func/src/domain/message-status.ts
@@ -1,11 +1,22 @@
 import * as z from "zod";
 
+//TODO: move this into a common package
+export const fiscalCodeSchema = z
+  .string()
+  .regex(
+    /^[A-Z]{6}[0-9LMNPQRSTUV]{2}[ABCDEHLMPRST][0-9LMNPQRSTUV]{2}[A-Z][0-9LMNPQRSTUV]{3}[A-Z]$/,
+    "Must be a valid Italian fiscal code",
+  )
+  .brand("FiscalCode");
+
+export type FiscalCode = z.infer<typeof fiscalCodeSchema>;
+
 const rejectionReasonSchema = z
   .enum(["SERVICE_NOT_ALLOWED", "USER_NOT_FOUND", "UNKNOWN"])
   .default("UNKNOWN");
 
 const rejectedMessageStatusSchema = z.object({
-  fiscalCode: z.string().min(1).optional(),
+  fiscalCode: fiscalCodeSchema,
   isArchived: z.boolean().default(false),
   isRead: z.boolean().default(false),
   messageId: z.string().min(1),
@@ -22,7 +33,7 @@ const notRejectedMessageStatusValueEnum = z.enum([
 ]);
 
 const notRejectedMessageStatusSchema = z.object({
-  fiscalCode: z.string().min(1).optional(),
+  fiscalCode: fiscalCodeSchema,
   isArchived: z.boolean().default(false),
   isRead: z.boolean().default(false),
   messageId: z.string().min(1),
