d209299d370e5832aa70fc8fca2d80edd0b0ff22
acuiuli <andrea.cuiuli@gmail.com>
2024-11-29 14:53:23 +0100

    added type to fiscalCode param

--- apps/etl-func/src/adapters/redis/recipient.ts
+++ apps/etl-func/src/adapters/redis/recipient.ts
@@ -1,4 +1,5 @@
 import { RecipientRepository } from "@/domain/interfaces/tokenizer.js";
+import { FiscalCode } from "@/domain/message-status.js";
 import {
   RedisClientType,
   RedisDefaultModules,
@@ -15,15 +16,15 @@ export default class RedisRecipientRepository implements RecipientRepository {
     this.#client = client;
   }
 
-  #key(fiscalCode: string) {
+  #key(fiscalCode: FiscalCode) {
     return `user:${fiscalCode.toLowerCase()}:recipient.id`;
   }
 
-  async get(id: string) {
-    return (await this.#client.get(this.#key(id))) || undefined;
+  async get(fiscalCode: FiscalCode) {
+    return (await this.#client.get(this.#key(fiscalCode))) || undefined;
   }
 
-  async upsert(fiscalCode: string, recipientId: string) {
+  async upsert(fiscalCode: FiscalCode, recipientId: string) {
     await this.#client.set(this.#key(fiscalCode), recipientId);
   }
 }
--- apps/etl-func/src/domain/interfaces/tokenizer.ts
+++ apps/etl-func/src/domain/interfaces/tokenizer.ts
@@ -1,7 +1,9 @@
+import { FiscalCode } from "../message-status.js";
+
 export interface TokenizerClient {
   tokenize(pii: string): Promise<string>;
 }
 export interface RecipientRepository {
-  get(fiscalCode: string): Promise<string | undefined>;
-  upsert(fiscalCode: string, recipientId: string): Promise<void>;
+  get(fiscalCode: FiscalCode): Promise<string | undefined>;
+  upsert(fiscalCode: FiscalCode, recipientId: string): Promise<void>;
 }
