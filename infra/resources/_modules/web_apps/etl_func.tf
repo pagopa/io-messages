a3735acf019c845d5d569130e8e2adcbbf4949a8
acuiuli <andrea.cuiuli@gmail.com>
2024-12-05 10:58:28 +0100

    fix: etl_func app settings

--- infra/resources/_modules/web_apps/etl_func.tf
+++ infra/resources/_modules/web_apps/etl_func.tf
@@ -16,19 +16,8 @@ module "etl_func" {
 
   health_check_path = "/api/health"
 
-  app_settings = {
-    NODE_ENV                       = "production",
-    FUNCTIONS_WORKER_RUNTIME       = "node",
-    MESSAGE_CONTENT_STORAGE_URI    = var.app_settings.message_content_storage_uri
-    EVENTHUB_CONNECTION_URI        = var.app_settings.eventhub_connection_uri,
-    MESSAGE_CONTENT_CONTAINER_NAME = "message-content",
-    MESSAGE_EVENTHUB_NAME          = "io-p-itn-com-etl-messages-evh-01"
-    PDV_TOKENIZER_API_KEY          = "@Microsoft.KeyVault(VaultName=${var.common_key_vault.name};SecretName=func-elt-PDV-TOKENIZER-API-KEY)"
-    PDV_TOKENIZER_BASE_URL         = "https://api.tokenizer.pdv.pagopa.it/tokenizer/v1"
-    COSMOS__accountEndpoint        = var.cosmosdb_account_api.endpoint
-    COSMOS_DBNAME                  = "db",
-    COSMOS_MESSAGES_CONTAINER_NAME = "messages-dataplan-ingestion-test"
-  }
+  app_settings = locals.etl_func.app_settings
+  slot_app_settings = locals.etl_func.app_settings
 
   sticky_app_setting_names = ["NODE_ENV"]
 
