d96bb8d910db6aa8338884da058c98b73c93a191
Garma <giovanniarmaleo00@gmail.com>
2024-12-12 10:50:28 +0100

    fix: add fiscal code in mocks

--- apps/etl-func/src/__mocks__/message-status.ts
+++ apps/etl-func/src/__mocks__/message-status.ts
@@ -1,5 +1,7 @@
 import { messageStatusSchema } from "@/domain/message-status.js";
 
+export const aFiscalCode = "RMLGNN97R06F158N";
+
 export const aValidMessageStatus = messageStatusSchema.parse({
   fiscalCode: aFiscalCode,
   id: "01JD4YVX03H45HPGAB3E0Y2658-0000000000000000",
