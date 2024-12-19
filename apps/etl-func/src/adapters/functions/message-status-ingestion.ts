0ab4b238d98f086c0cf7ada1dd82a8c3131a677c
Garma <giovanniarmaleo00@gmail.com>
2024-12-19 10:34:54 +0100

    refactor: remove parseMessageStatus in favour of in place definition

--- apps/etl-func/src/adapters/functions/message-status-ingestion.ts
+++ apps/etl-func/src/adapters/functions/message-status-ingestion.ts
@@ -2,15 +2,13 @@ import { MessageStatus, messageStatusSchema } from "@/domain/message-status.js";
 import { IngestMessageStatusUseCase } from "@/domain/use-cases/ingest-message-status.js";
 import { CosmosDBHandler } from "@azure/functions";
 
-const parseMessageStatus = (input: unknown): MessageStatus | undefined => {
-  const { data, success } = messageStatusSchema.safeParse(input);
-  return success ? data : undefined;
-};
-
 const messageStatusIngestionHandler =
   (ingestUseCase: IngestMessageStatusUseCase): CosmosDBHandler =>
   async (documents: unknown[]) => {
-    const parsedMessageStatusBatch = documents.map(parseMessageStatus);
+    const parsedMessageStatusBatch = documents.map((input) => {
+      const { data, success } = messageStatusSchema.safeParse(input);
+      return success ? data : undefined;
+    });
     const messageStatusBatch = parsedMessageStatusBatch.filter(
       (status): status is MessageStatus => status !== undefined,
     );
