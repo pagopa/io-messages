b0866457e0c6af3c6f3fd1193b3bdbc81f4ffb22
Garma <giovanniarmaleo00@gmail.com>
2025-01-24 11:19:35 +0100

    fix: regex for cosmos account uri

--- apps/etl-func/src/adapters/cosmos/config.ts
+++ apps/etl-func/src/adapters/cosmos/config.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const cosmosConfigSchema = z.object({
-  accountUri: z.string().regex(/^[a-zA-Z0-9-]+\.servicebus\.windows\.net$/),
+  accountUri: z.string().url(),
   databaseName: z.string().min(1),
   messageIngestionSummaryCollectionName: z.string().min(1),
 });
