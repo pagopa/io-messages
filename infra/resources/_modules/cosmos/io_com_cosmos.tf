a57497309910688174f30e94e13a65c9f8cd837c
Garma <64153597+Garma00@users.noreply.github.com>
2025-01-30 16:24:02 +0100

    feat(infra): add new cosmos account with databases (#88)
    Co-authored-by: Krusty93 <andrea.grillo@pagopa.it>

---
+++ infra/resources/_modules/cosmos/io_com_cosmos.tf
@@ -0,0 +1,80 @@
+module "io_com_cosmos_account" {
+  source  = "pagopa/dx-azure-cosmos-account/azurerm"
+  version = "~> 0"
+
+  subnet_pep_id = var.subnet_pep_id
+
+  alerts = {
+    enabled         = true
+    action_group_id = var.action_group_id
+    thresholds = {
+      provisioned_throughput_exceeded = 1000
+    }
+  }
+
+  environment = var.environment
+
+  secondary_geo_locations = [
+    {
+      location          = "spaincentral"
+      failover_priority = 1
+      zone_redundant    = false
+    }
+  ]
+
+  tags = var.tags
+
+  consistency_policy = {
+    consistency_preset = "Default"
+  }
+
+  resource_group_name = var.resource_group
+}
+
+resource "azurerm_cosmosdb_sql_database" "data_lake" {
+  name                = "data-lake-cosmos-01"
+  resource_group_name = var.resource_group
+  account_name        = module.io_com_cosmos_account.name
+}
+
+resource "azurerm_cosmosdb_sql_database" "remote_content" {
+  name                = "remote-content-cosmos-01"
+  resource_group_name = var.resource_group
+  account_name        = module.io_com_cosmos_account.name
+}
+
+resource "azurerm_cosmosdb_sql_container" "messages_summary" {
+  name                = "messages-summary"
+  resource_group_name = var.resource_group
+
+  account_name        = module.io_com_cosmos_account.name
+  database_name       = azurerm_cosmosdb_sql_database.data_lake.name
+  partition_key_paths = ["/year"]
+}
+
+resource "azurerm_cosmosdb_sql_container" "message_configuration" {
+  name                = "message-configuration"
+  resource_group_name = var.resource_group
+
+  account_name        = module.io_com_cosmos_account.name
+  database_name       = azurerm_cosmosdb_sql_database.remote_content.name
+  partition_key_paths = ["/configurationId"]
+}
+
+resource "azurerm_cosmosdb_sql_container" "user_configuration" {
+  name                = "user-configuration"
+  resource_group_name = var.resource_group
+
+  account_name        = module.io_com_cosmos_account.name
+  database_name       = azurerm_cosmosdb_sql_database.remote_content.name
+  partition_key_paths = ["/userId"]
+}
+
+resource "azurerm_cosmosdb_sql_container" "remote_content_lease" {
+  name                = "remote-content-lease"
+  resource_group_name = var.resource_group
+
+  account_name        = module.io_com_cosmos_account.name
+  database_name       = azurerm_cosmosdb_sql_database.remote_content.name
+  partition_key_paths = ["/id"]
+}
--- infra/resources/prod/main.tf
+++ infra/resources/prod/main.tf
@@ -223,4 +223,15 @@ module "monitoring" {
 module "cosmos" {
   source           = "../_modules/cosmos"
   cosmosdb_account = data.azurerm_cosmosdb_account.cosmos_api
+  tags             = local.tags
+  resource_group   = "io-p-itn-com-rg-01"
+  action_group_id  = module.monitoring.action_group.io_com_error_id
+  subnet_pep_id    = data.azurerm_subnet.pep.id
+  environment = {
+    prefix          = local.prefix
+    env_short       = local.env_short
+    location        = local.location
+    app_name        = "com"
+    instance_number = "01"
+  }
 }
