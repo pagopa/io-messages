db34d485b84b62eaf929beb11a1bce180805e82a
acuiuli <andrea.cuiuli@gmail.com>
2025-01-31 16:20:38 +0100

    fixed TelemtryEventName enum

--- apps/etl-func/src/domain/telemetry.ts
+++ apps/etl-func/src/domain/telemetry.ts
@@ -2,7 +2,7 @@ export interface TelemetryService {
   trackEvent: (name: string, properties: object) => void;
 }
 
-export declare enum TelemetryEventName {
+export enum TelemetryEventName {
   CONTENT_NOT_FOUND = "io.com.message_ingestion.content_not_found",
   EXECUTION_ERROR = "io.com.message_ingestion.execution_error",
   MALFORMED_DOCUMENTS = "io.com.message_ingestion.malformed_documents",
