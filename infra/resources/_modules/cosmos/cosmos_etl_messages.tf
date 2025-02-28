ea9567bebdb5b1fc0c2fbc043c9e02c147eb8009
Luca Mancosu <luca.mancosu@it.ey.com>
2025-02-28 10:36:52 +0100

    removing an useless container creation

--- infra/resources/_modules/cosmos/cosmos_etl_messages.tf
+++ infra/resources/_modules/cosmos/cosmos_etl_messages.tf
@@ -1,16 +1,3 @@
-module "comsosdb_sql_container_messages_ingestion_test" {
-  source = "github.com/pagopa/terraform-azurerm-v4//cosmosdb_sql_container?ref=v1.2.1"
-
-  name                = "messages"
-  resource_group_name = var.cosmosdb_account.resource_group_name
-
-  account_name  = var.cosmosdb_account.name
-  database_name = "db"
-
-  partition_key_paths = ["/fiscalCode"]
-  default_ttl         = "-1"
-}
-
 module "comsosdb_sql_container_messages_ingestion_test_lease" {
   source = "github.com/pagopa/terraform-azurerm-v4//cosmosdb_sql_container?ref=v1.2.1"
 
