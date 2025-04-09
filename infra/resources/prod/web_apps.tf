cc9e21e9940ffed46cff8ad00b0c5a2b02598df9
Luca Mancosu <luca.mancosu@it.ey.com>
2025-04-09 10:41:18 +0200

    change push_notif_func cidr

--- infra/resources/prod/web_apps.tf
+++ infra/resources/prod/web_apps.tf
@@ -26,7 +26,7 @@ module "web_apps" {
     notif_func      = "10.20.8.0/26"
     etl_func        = "10.20.8.0/26"
     citizen_func    = "10.20.8.64/26"
-    push_notif_func = "10.20.8.0/26"
+    push_notif_func = "10.20.10.64/26"
   }
 
   nat_gateway_id = data.azurerm_nat_gateway.itn_ng.id
