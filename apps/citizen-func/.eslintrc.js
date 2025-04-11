b7df858df603109cee0f0b49336dc77e8c340832
Luca Mancosu <luca.mancosu@it.ey.com>
2025-04-11 10:51:15 +0200

    enable legaxy linting

--- apps/citizen-func/.eslintrc.js
+++ apps/citizen-func/.eslintrc.js
@@ -1,27 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "ignorePatterns": [
-        "node_modules",
-        "generated",
-        "**/__tests__/*",
-        "**/__mocks__/*",
-        "**/__integrations__/*",
-        "*.d.ts",
-        "docker"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": "tsconfig.json",
-        "sourceType": "module"
-    },
-    "extends": [
-        "@pagopa/eslint-config/strong",
-    ],
-    "rules": {
-        
-    }
-}
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  ignorePatterns: [
+    "node_modules",
+    "generated",
+    "**/__tests__/*",
+    "**/__mocks__/*",
+    "**/__integrations__/*",
+    "*.d.ts",
+    "docker",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+  },
+  extends: ["@pagopa/eslint-config/strong"],
+  rules: {
+    "no-invalid-this": "off",
+    "@typescript-eslint/no-invalid-this": "off",
+  },
+};
