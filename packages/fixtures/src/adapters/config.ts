43cb5eea71c37eff060f33f2566c25e9cc3c4a1e
garma <giovanniarmaleo00@gmail.com>
2025-04-15 14:34:20 +0200

    remove booleanFromString

--- packages/fixtures/src/adapters/config.ts
+++ packages/fixtures/src/adapters/config.ts
@@ -1,18 +1,12 @@
 import { DefaultAzureCredential } from "@azure/identity";
 import { z } from "zod";
 
-const booleanFromString = z
-  .string()
-  .toLowerCase()
-  .transform((x) => x === "true")
-  .pipe(z.boolean().default(false));
-
 export const loadFixturesOptionsSchema = z.object({
   cosmosDatabaseName: z.string().min(1),
   cosmosEndpoint: z.string().url(),
   cosmosMessageContainerName: z.string().min(1),
-  includePayments: booleanFromString,
-  includeRemoteContents: booleanFromString,
+  includePayments: z.boolean().default(false),
+  includeRemoteContents: z.boolean().default(false),
   storageConnectionString: z.string().min(1),
   storageMessageContentContainerName: z.string().min(1),
 });
