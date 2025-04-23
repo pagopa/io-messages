9ba8f66e8892969febd5875c9845544d5f866464
garma <giovanniarmaleo00@gmail.com>
2025-04-23 09:20:41 +0200

    add log information in health use-case

--- apps/ops-func/src/domain/use-cases/health.ts
+++ apps/ops-func/src/domain/use-cases/health.ts
@@ -1,4 +1,4 @@
-import { Database } from "@azure/cosmos";
+import { Database, ErrorResponse } from "@azure/cosmos";
 
 export class HealthUseCase {
   db: Database;
@@ -14,6 +14,12 @@ export class HealthUseCase {
         body: "it works!",
       };
     } catch (error) {
+      if (error instanceof ErrorResponse) {
+        return {
+          body: `Service connection failed: ${error.name} code: ${error.code} cause: ${error.cause}`,
+          status: 500,
+        };
+      }
       return {
         body: "Service connection failed",
         status: 500,
