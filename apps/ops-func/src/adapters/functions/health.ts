ee0f4ca516dccbdc467d129a39feb53dfd63e6eb
garma <giovanniarmaleo00@gmail.com>
2025-04-28 10:46:52 +0200

    return failures in healthcheck func

--- apps/ops-func/src/adapters/functions/health.ts
+++ apps/ops-func/src/adapters/functions/health.ts
@@ -5,14 +5,8 @@ export const healthcheck =
   (healthUseCase: HealthUseCase): HttpHandler =>
   async () => {
     const failures = await healthUseCase.execute();
-    if (failures.length > 0) {
-      return {
-        body: `Service connection failed, cause: ${failures}`,
-        status: 500,
-      };
-    }
     return {
-      body: "It works!",
-      status: 200,
+      jsonBody: failures,
+      status: failures.length > 0 ? 500 : 200,
     };
   };
