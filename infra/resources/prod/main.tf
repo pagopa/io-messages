9f3cb4abbf98e5e7774b3ac73996584873a006e8
garma <giovanniarmaleo00@gmail.com>
2025-04-29 10:53:21 +0200

    move cosmos outside main.tf

--- infra/resources/prod/main.tf
+++ infra/resources/prod/main.tf
@@ -120,19 +120,3 @@ module "monitoring" {
   resource_group_name = azurerm_resource_group.itn_com.name
   io_com_slack_email  = data.azurerm_key_vault_secret.io_com_slack_email.value
 }
-
-module "cosmos" {
-  source           = "../_modules/cosmos"
-  cosmosdb_account = data.azurerm_cosmosdb_account.cosmos_api
-  tags             = local.tags
-  resource_group   = "io-p-itn-com-rg-01"
-  action_group_id  = module.monitoring.action_group.io_com_error_id
-  subnet_pep_id    = data.azurerm_subnet.pep.id
-  environment = {
-    prefix          = local.prefix
-    env_short       = local.env_short
-    location        = local.location
-    app_name        = "com"
-    instance_number = "01"
-  }
-}
