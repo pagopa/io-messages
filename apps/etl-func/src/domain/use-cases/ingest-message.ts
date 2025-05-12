89854ffd82a6473cfbea0688f0c48bd4aaa30335
garma <giovanniarmaleo00@gmail.com>
2025-05-12 11:10:06 +0200

    Remove call to summary collect

--- apps/etl-func/src/domain/use-cases/ingest-message.ts
+++ apps/etl-func/src/domain/use-cases/ingest-message.ts
@@ -8,19 +8,16 @@ import { TokenizerClient } from "../tokenizer.js";
 
 export class IngestMessageUseCase {
   #eventProducer: EventProducer<MessageEvent>;
-  #eventSummaryCollector: EventCollector;
   #messageRepo: MessageRepository;
   #tokenizer: TokenizerClient;
 
   constructor(
     messageAdapter: MessageRepository,
     tokenizer: TokenizerClient,
-    eventSummaryCollector: EventCollector,
     eventProducer: EventProducer<MessageEvent>,
   ) {
     this.#messageRepo = messageAdapter;
     this.#tokenizer = tokenizer;
-    this.#eventSummaryCollector = eventSummaryCollector;
     this.#eventProducer = eventProducer;
   }
 
@@ -43,7 +40,6 @@ export class IngestMessageUseCase {
 
     if (messagesEvent.length > 0) {
       await this.#eventProducer.publish(messagesEvent);
-      await this.#eventSummaryCollector.collect(messagesEvent.length);
     }
   }
 }
