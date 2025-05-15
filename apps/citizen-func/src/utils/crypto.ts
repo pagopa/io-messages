2fca88f2ed70da204bdf37131654e2fd03f5e272
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-05-15 13:11:43 +0000

    Remove unused code

--- apps/citizen-func/src/utils/crypto.ts
+++ apps/citizen-func/src/utils/crypto.ts
@@ -6,6 +6,3 @@ import * as crypto from "crypto";
 
 export const toHash = (s: string): string =>
   crypto.createHash("sha256").update(s).digest("hex");
-
-export const randomBytes = (size: number): string =>
-  crypto.randomBytes(size).toString("hex");
