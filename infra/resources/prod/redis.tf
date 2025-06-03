8a21aaaf391a557fc3045c2c594334f35b877296
Luca Mancosu <luca.mancosu@it.ey.com>
2025-06-03 15:52:06 +0200

    add a new redis inside io-p-itn-com-rg-01

--- infra/resources/prod/redis.tf
+++ infra/resources/prod/redis.tf
@@ -44,3 +44,51 @@ module "redis_messages" {
 
   tags = local.tags
 }
+
+
+module "com_redis_messages" {
+  source = "github.com/pagopa/terraform-azurerm-v4//redis_cache?ref=v1.2.1"
+
+  name                = "${local.project}-${local.domain}-redis-01"
+  resource_group_name = azurerm_resource_group.itn_com.name
+  location            = azurerm_resource_group.itn_com.location
+
+  capacity              = 2
+  family                = "C"
+  sku_name              = "Standard"
+  redis_version         = "6"
+  enable_authentication = true
+  zones                 = [1, 2]
+
+  // when azure can apply patch?
+  patch_schedules = [{
+    day_of_week    = "Sunday"
+    start_hour_utc = 23
+    },
+    {
+      day_of_week    = "Monday"
+      start_hour_utc = 23
+    },
+    {
+      day_of_week    = "Tuesday"
+      start_hour_utc = 23
+    },
+    {
+      day_of_week    = "Wednesday"
+      start_hour_utc = 23
+    },
+    {
+      day_of_week    = "Thursday"
+      start_hour_utc = 23
+    },
+  ]
+
+  private_endpoint = {
+    enabled              = true
+    subnet_id            = data.azurerm_subnet.pep.id
+    virtual_network_id   = data.azurerm_virtual_network.vnet_common_itn.id
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.privatelink_redis_cache.id]
+  }
+
+  tags = local.tags
+}
