f23333e833e54b291b7511ebb87e6443d712a6b0
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-06-13 15:41:46 +0000

    Configure appinsights

--- apps/reminder/src/main/java/it/ioapp/com/reminder/Application.java
+++ apps/reminder/src/main/java/it/ioapp/com/reminder/Application.java
@@ -1,5 +1,6 @@
 package it.ioapp.com.reminder;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String[] args) {
+    ApplicationInsights.attach();
     SpringApplication.run(Application.class, args);
   }
 }
