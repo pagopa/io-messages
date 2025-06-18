c906ef5dfa06ed7de3cc62559d369a4795dc420f
Luca Mancosu <luca.mancosu@it.ey.com>
2025-06-18 17:20:07 +0200

    import reminder subscription

--- infra/resources/_modules/apim/apim_subscriptions.tf
+++ infra/resources/_modules/apim/apim_subscriptions.tf
@@ -1,3 +1,20 @@
+resource "azurerm_api_management_subscription" "reminder_itn" {
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  user_id             = azurerm_api_management_user.reminder_user_itn.id
+  product_id          = azurerm_api_management_product.apim_itn_product_notifications.id
+  display_name        = "Reminder API"
+  state               = "active"
+  allow_tracing       = false
+}
+
+resource "azurerm_key_vault_secret" "reminder_subscription_primary_key_itn" {
+  name         = "${format("%s-reminder", local.product)}-subscription-key-itn"
+  value        = azurerm_api_management_subscription.reminder_itn.primary_key
+  content_type = "subscription key"
+  key_vault_id = data.azurerm_key_vault.messages_key_vault.id
+}
+
 resource "azurerm_api_management_subscription" "payment_updater_reminder_itn" {
   api_management_name = data.azurerm_api_management.apim_itn_api.name
   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
