e4dedfad1acaedf39c8ead7d76b77b2ba23c7710
Luca Mancosu <luca.mancosu@it.ey.com>
2025-06-18 12:39:10 +0200

    change apim files name

---
+++ infra/resources/_modules/apim/apim_products.tf
@@ -0,0 +1,22 @@
+resource "azurerm_api_management_product" "apim_itn_product_notifications" {
+  product_id   = "io-notifications-api"
+  display_name = "IO NOTIFICATIONS API"
+  description  = "Product for IO notifications"
+
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+
+  subscription_required = true
+  approval_required     = false
+  published             = true
+}
+
+resource "azurerm_api_management_product_policy" "apim_itn_product_notifications_policy" {
+  product_id          = azurerm_api_management_product.apim_itn_product_notifications.product_id
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+
+  xml_content = file("../_modules/apim/api_product/messages/_base_policy.xml")
+
+
+}
