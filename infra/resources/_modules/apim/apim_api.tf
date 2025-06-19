3f477c01919f2547f24e983add599ce01c551f1b
Luca Mancosu <luca.mancosu@it.ey.com>
2025-06-19 14:03:05 +0200

    import message sending internal api

--- infra/resources/_modules/apim/apim_api.tf
+++ infra/resources/_modules/apim/apim_api.tf
@@ -37,7 +37,7 @@ resource "azurerm_api_management_product_api" "io_backend_notification_itn_api_v
 }
 
 
-#SENDING FUNC API
+#SENDING FUNC API - EXTERNAL
 
 resource "azurerm_api_management_api" "messages_sending_external_api_v1" {
   name                = format("%s-%s-messages-sending-external-api-01", local.product, var.legacy_location_short)
@@ -54,7 +54,7 @@ resource "azurerm_api_management_api" "messages_sending_external_api_v1" {
 
   import {
     content_format = "openapi"
-    content_value  = file("../_modules/apim/api/messages-sending/v1/_index_external.yaml")
+    content_value  = file("../../../apps/sending-func/openapi/index_external.yaml")
   }
 }
 
@@ -67,9 +67,50 @@ resource "azurerm_api_management_api_policy" "messages_sending_external_api_v1_p
 }
 
 resource "azurerm_api_management_product_api" "messages_sending_external_api_v1_product_api" {
-
   product_id          = data.azurerm_api_management_product.apim_itn_product_services.product_id
   api_name            = azurerm_api_management_api.messages_sending_external_api_v1.name
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+}
+
+
+# SENDING FUNC API - INTERNAL
+
+resource "azurerm_api_management_api" "messages_sending_internal_api_v1" {
+  name                = format("%s-%s-messages-sending-internal-api-01", local.product, var.legacy_location_short)
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+  revision            = "1"
+
+  description  = "IO Messages Sending - Internal - API"
+  display_name = "IO Messages Sending - Internal - API"
+  api_type     = local.apim_defult_api_type
+
+  path      = "api/v1/messages-sending/internal"
+  protocols = ["https"]
+
+  subscription_required = true
+  service_url           = null
+
+
+  import {
+    content_format = "openapi"
+    content_value  = file("../../../apps/sending-func/openapi/index.yaml")
+  }
+
+}
+
+resource "azurerm_api_management_api_policy" "messages_sending_internal_api_v1_policy" {
+  api_name            = azurerm_api_management_api.this.name
+  api_management_name = data.azurerm_api_management.apim_itn_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
+
+  xml_content = file("./api/messages-sending/v1/_base_policy_internal.xml")
+}
+
+resource "azurerm_api_management_product_api" "messages_sending_internal_api_v1_product_api" {
+  product_id          = azurerm_api_management_product.apim_itn_product_notifications.product_id
+  api_name            = azurerm_api_management_api.messages_sending_internal_api_v1.name
   api_management_name = data.azurerm_api_management.apim_itn_api.name
   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
 }
