260f9508a2dc4ff787521d6e7ca49e492a42f38c
Luca Mancosu <luca.mancosu@it.ey.com>
2025-06-23 15:23:12 +0200

    remove unused api

--- infra/resources/_modules/apim/notification.tf
+++ infra/resources/_modules/apim/notification.tf
@@ -20,45 +20,6 @@ resource "azurerm_api_management_product_policy" "apim_itn_product_notifications
 }
 
 
-
-# BACKEND NOTIFICAZTION API
-resource "azurerm_api_management_api" "io_backend_notification_itn_api_v1" {
-  name                = format("%s-io-backend-notification-api", local.product)
-  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-  api_management_name = data.azurerm_api_management.apim_itn_api.name
-  revision            = "1"
-  display_name        = "IO Backend - Notification API"
-  description         = "IO Backend - Notification API"
-
-  path                  = "io-backend-notification/api/v1"
-  protocols             = ["https"]
-  service_url           = null
-  subscription_required = false
-
-  import {
-    content_format = "openapi"
-    content_value  = file("../_modules/apim/api/io-backend-notification/v1/_openapi.yaml")
-  }
-}
-
-resource "azurerm_api_management_api_policy" "io_backend_notification_itn_api_v1_policy" {
-  api_name            = azurerm_api_management_api.io_backend_notification_itn_api_v1.name
-  api_management_name = data.azurerm_api_management.apim_itn_api.name
-  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-
-  xml_content = file("../_modules/apim/api/io-backend-notification/v1/_base_policy.xml")
-}
-
-
-resource "azurerm_api_management_product_api" "io_backend_notification_itn_api_v1_product_api" {
-  product_id          = azurerm_api_management_product.apim_itn_product_notifications.product_id
-  api_name            = azurerm_api_management_api.io_backend_notification_itn_api_v1.name
-  api_management_name = azurerm_api_management_api.io_backend_notification_itn_api_v1.api_management_name
-  resource_group_name = azurerm_api_management_api.io_backend_notification_itn_api_v1.resource_group_name
-}
-
-
-
 # SENDING FUNC API - INTERNAL
 
 resource "azurerm_api_management_api" "messages_sending_internal_api_v1" {
@@ -94,46 +55,6 @@ resource "azurerm_api_management_api_policy" "messages_sending_internal_api_v1_p
 resource "azurerm_api_management_product_api" "messages_sending_internal_api_v1_product_api" {
   product_id          = azurerm_api_management_product.apim_itn_product_notifications.product_id
   api_name            = azurerm_api_management_api.messages_sending_internal_api_v1.name
-  api_management_name = data.azurerm_api_management.apim_itn_api.name
-  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-}
-
-
-# SERVICE MESSSAGE API - INTERNAL
-
-resource "azurerm_api_management_api" "service_messages_internal_api_v1" {
-  name                = format("%s-service-messages-internal-api", local.product)
-  api_management_name = data.azurerm_api_management.apim_itn_api.name
-  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-  revision            = "1"
-
-  description  = "IO Service Messages - Internal - API"
-  display_name = "IO Service Messages - Internal - API"
-
-  path                  = "service-messages/api/v1"
-  protocols             = ["https"]
-  subscription_required = true
-  service_url           = null
-
-  import {
-    content_format = "openapi"
-    content_value  = file("../_modules/apim/api/service-messages/_index_internal.yaml")
-  }
-
-}
-
-resource "azurerm_api_management_api_policy" "service_messages_internal_api_v1_policy" {
-
-  api_name            = azurerm_api_management_api.service_messages_internal_api_v1.name
-  api_management_name = data.azurerm_api_management.apim_itn_api.name
-  resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-
-  xml_content = file("../_modules/apim/api/service-messages/v1/_base_policy.xml")
-}
-
-resource "azurerm_api_management_product_api" "service_messages_internal_api_v1_product_api" {
-  product_id          = azurerm_api_management_product.apim_itn_product_notifications.product_id
-  api_name            = azurerm_api_management_api.service_messages_internal_api_v1.name
   api_management_name = data.azurerm_api_management.apim_itn_api.name
   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
 }
--- infra/resources/prod/import.tf
+++ infra/resources/prod/import.tf
@@ -249,25 +249,6 @@ import {
   id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-common-rg-01/providers/Microsoft.ApiManagement/service/io-p-itn-apim-01/products/io-notifications-api"
 }
 
-
-##APIM NOTIFICATIONS API
-
-#backend notification API
-import {
-  to = module.apim.azurerm_api_management_api.io_backend_notification_itn_api_v1
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-common-rg-01/providers/Microsoft.ApiManagement/service/io-p-itn-apim-01/apis/io-p-io-backend-notification-api"
-}
-
-import {
-  to = module.apim.azurerm_api_management_api_policy.io_backend_notification_itn_api_v1_policy
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-common-rg-01/providers/Microsoft.ApiManagement/service/io-p-itn-apim-01/apis/io-p-io-backend-notification-api"
-}
-
-import {
-  to = module.apim.azurerm_api_management_product_api.io_backend_notification_itn_api_v1_product_api
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-common-rg-01/providers/Microsoft.ApiManagement/service/io-p-itn-apim-01/products/io-notifications-api/apis/io-p-io-backend-notification-api"
-}
-
 #sending external API
 import {
   to = module.apim.azurerm_api_management_api.messages_sending_external_api_v1
@@ -314,22 +295,6 @@ import {
 import {
   to = module.apim.azurerm_api_management_product_api.service_messages_manage_api_v1_product_api
   id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-common-rg-01/providers/Microsoft.ApiManagement/service/io-p-itn-apim-01/products/io-services-api/apis/io-p-service-messages-manage-api"
-}
-
-#service internal API
-import {
-  to = module.apim.azurerm_api_management_api.service_messages_internal_api_v1
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-common-rg-01/providers/Microsoft.ApiManagement/service/io-p-itn-apim-01/apis/io-p-service-messages-internal-api"
-}
-
-import {
-  to = module.apim.azurerm_api_management_api_policy.service_messages_internal_api_v1_policy
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-common-rg-01/providers/Microsoft.ApiManagement/service/io-p-itn-apim-01/apis/io-p-service-messages-internal-api"
-}
-
-import {
-  to = module.apim.azurerm_api_management_product_api.service_messages_internal_api_v1_product_api
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-common-rg-01/providers/Microsoft.ApiManagement/service/io-p-itn-apim-01/products/io-notifications-api/apis/io-p-service-messages-internal-api"
 }
 
 
