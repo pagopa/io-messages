97a1191b532e685b279ff5e43a836924e9e7977c
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-06-25 13:45:35 +0000

    Remove unnecessary imports

--- infra/resources/prod/import.tf
+++ infra/resources/prod/import.tf
@@ -1,204 +1,3 @@
-import {
-  to = azurerm_resource_group.notifications
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-notifications"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_namespace.sandbox_partition_1
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-notifications/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-sandbox-partition-1"
-}
-
-import {
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-notifications/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-sandbox"
-  to = module.notification_hubs_weu.azurerm_notification_hub_namespace.sandbox
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_namespace.common_partition_4
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-4"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_namespace.common_partition_3
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-3"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_namespace.common_partition_2
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-2"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_namespace.common_partition_1
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-1"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_namespace.common
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.sandbox_partition_1_default_listen
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-notifications/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-sandbox-partition-1/notificationHubs/io-p-ntf-sandbox-partition-1/authorizationRules/DefaultListenSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.sandbox_partition_1_default_full
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-notifications/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-sandbox-partition-1/notificationHubs/io-p-ntf-sandbox-partition-1/authorizationRules/DefaultFullSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.sandbox_default_listen
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-notifications/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-sandbox/notificationHubs/io-p-ntf-sandbox/authorizationRules/DefaultListenSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.sandbox_default_full
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-notifications/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-sandbox/notificationHubs/io-p-ntf-sandbox/authorizationRules/DefaultFullSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.common_partition_4_default_listen
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-4/notificationHubs/io-p-ntf-common-partition-4/authorizationRules/DefaultListenSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.common_partition_4_default_full
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-4/notificationHubs/io-p-ntf-common-partition-4/authorizationRules/DefaultFullSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.common_partition_3_default_listen
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-3/notificationHubs/io-p-ntf-common-partition-3/authorizationRules/DefaultListenSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.common_partition_3_default_full
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-3/notificationHubs/io-p-ntf-common-partition-3/authorizationRules/DefaultFullSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.common_partition_2_default_listen
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-2/notificationHubs/io-p-ntf-common-partition-2/authorizationRules/DefaultListenSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.common_partition_2_default_full
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-2/notificationHubs/io-p-ntf-common-partition-2/authorizationRules/DefaultFullSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.common_partition_1_default_listen
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-1/notificationHubs/io-p-ntf-common-partition-1/authorizationRules/DefaultListenSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.common_partition_1_default_full
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-1/notificationHubs/io-p-ntf-common-partition-1/authorizationRules/DefaultFullSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.common_default_listen
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common/notificationHubs/io-p-ntf-common/authorizationRules/DefaultListenSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub_authorization_rule.common_default_full
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common/notificationHubs/io-p-ntf-common/authorizationRules/DefaultFullSharedAccessSignature"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub.sandbox_partition_1
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-notifications/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-sandbox-partition-1/notificationHubs/io-p-ntf-sandbox-partition-1"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub.sandbox
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-notifications/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-sandbox/notificationHubs/io-p-ntf-sandbox"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub.common_partition_4
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-4/notificationHubs/io-p-ntf-common-partition-4"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub.common_partition_3
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-3/notificationHubs/io-p-ntf-common-partition-3"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub.common_partition_2
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-2/notificationHubs/io-p-ntf-common-partition-2"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub.common_partition_1
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common-partition-1/notificationHubs/io-p-ntf-common-partition-1"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub.common01
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common/notificationHubs/io-p-ntf-common01"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_notification_hub.common
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.NotificationHubs/namespaces/io-p-ntfns-common/notificationHubs/io-p-ntf-common"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_monitor_metric_alert.alert_nh_common_pns_errors
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.Insights/metricAlerts/[IOCOM|NHLegacy] Push Notification Service errors"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_monitor_metric_alert.alert_nh_common_partition_4_pns_errors
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.Insights/metricAlerts/[IOCOM|NH4] Push Notification Service errors"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_monitor_metric_alert.alert_nh_common_partition_4_anomalous_pns_success_volume
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.Insights/metricAlerts/[IOCOM|NH4] Push Notification Service anomalous success volume"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_monitor_metric_alert.alert_nh_common_partition_3_pns_errors
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.Insights/metricAlerts/[IOCOM|NH3] Push Notification Service errors"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_monitor_metric_alert.alert_nh_common_partition_3_anomalous_pns_success_volume
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.Insights/metricAlerts/[IOCOM|NH3] Push Notification Service anomalous success volume"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_monitor_metric_alert.alert_nh_common_partition_2_pns_errors
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.Insights/metricAlerts/[IOCOM|NH2] Push Notification Service errors"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_monitor_metric_alert.alert_nh_common_partition_2_anomalous_pns_success_volume
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.Insights/metricAlerts/[IOCOM|NH2] Push Notification Service anomalous success volume"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_monitor_metric_alert.alert_nh_common_anomalous_pns_success_volume
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.Insights/metricAlerts/[IOCOM|NHLegacy] Push Notification Service anomalous success volume"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_monitor_metric_alert.alert_nh_common_partition_1_anomalous_pns_success_volume
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.Insights/metricAlerts/[IOCOM|NH1] Push Notification Service anomalous success volume"
-}
-
-import {
-  to = module.notification_hubs_weu.azurerm_monitor_metric_alert.alert_nh_common_partition_1_pns_errors
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.Insights/metricAlerts/[IOCOM|NH1] Push Notification Service errors"
-}
-
-
 ####ITN APIM CONFIG IMPORTS####
 
 #APIM GROUPS
@@ -281,7 +80,6 @@ import {
   id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-common-rg-01/providers/Microsoft.ApiManagement/service/io-p-itn-apim-01/products/io-notifications-api/apis/io-p-weu-messages-sending-internal-api-01"
 }
 
-
 #APIM USERS
 
 import {
@@ -311,11 +109,11 @@ import {
 #APIM SECRETS
 import {
   to = module.apim.azurerm_key_vault_secret.reminder_subscription_primary_key_itn
-  id = "https://io-p-messages-kv.vault.azure.net/secrets/io-p-reminder-payment-api-subscription-key-itn/325879ffaa2a419a8a09a00814e087ed"
+  id = "https://io-p-itn-com-kv-01.vault.azure.net/secrets/reminder-notification-subscription-key/e5e1f0f304ed45d28b67a2b004fdab13"
 }
 import {
   to = module.apim.azurerm_key_vault_secret.reminder_payment_api_subscription_primary_key_itn
-  id = "https://io-p-messages-kv.vault.azure.net/secrets/io-p-reminder-payment-api-subscription-key-itn/7a0c56f927244b94a9a0b4abe50af030"
+  id = "https://io-p-itn-com-kv-01.vault.azure.net/secrets/reminder-paymentup-subscription-key/f1579fdf8cff44c484c840723d73d0c0"
 }
 
 
