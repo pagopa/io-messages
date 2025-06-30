f5a35f848f1fef47380a68d9775840aa8733d69b
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-06-30 08:47:22 +0000

    Fix

--- infra/resources/_modules/container_apps/data.tf
+++ infra/resources/_modules/container_apps/data.tf
@@ -2,8 +2,3 @@ data "azurerm_key_vault_secret" "pagopa_ecommerce_key" {
   name         = "pagopa-ecommerce-prod-subscription-key"
   key_vault_id = var.key_vault_id
 }
-
-data "azurerm_key_vault_secret" "pagopa_ecommerce_key" {
-  name         = "pagopa-ecommerce-prod-subscription-key"
-  key_vault_id = var.key_vault_id
-}
