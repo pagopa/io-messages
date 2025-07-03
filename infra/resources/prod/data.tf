d438acabcd08f4d50bf13d6e7b7d561112a1fd94
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-07-03 16:08:31 +0000

    Remove unused data

--- infra/resources/prod/data.tf
+++ infra/resources/prod/data.tf
@@ -86,11 +86,6 @@ data "azurerm_storage_container" "messages_content_container" {
   storage_account_name = data.azurerm_storage_account.storage_api.name
 }
 
-data "azurerm_storage_account" "storage_push_notifications" {
-  name                = replace(format("%s-weu-messages-notifst", local.project_legacy), "-", "")
-  resource_group_name = local.notifications_rg_name
-}
-
 data "azurerm_user_assigned_identity" "infra_ci_01" {
   name                = "${local.project}-msgs-infra-github-ci-id-01"
   resource_group_name = local.legacy_itn_rg_name
