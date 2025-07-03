41a4eb3eafde2a06a16b07a1b0033713a095962a
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-07-03 16:01:10 +0000

    Link mongo peps with private dns zone

--- infra/resources/_modules/mongo/payments.tf
+++ infra/resources/_modules/mongo/payments.tf
@@ -54,6 +54,11 @@ resource "azurerm_private_endpoint" "payments_cosmos_account" {
     is_manual_connection           = false
     subresource_names              = ["MongoDB"]
   }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.privatelink_mongo_cosmos_azure_com.id]
+  }
 }
 
 resource "azurerm_cosmosdb_mongo_database" "db_payments" {
