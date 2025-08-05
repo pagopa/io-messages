a606b0f07a9c448e7a64177322595fb68bf96fd0
acuiuli <andrea.cuiuli@gmail.com>
2025-08-05 15:49:09 +0200

    fix typecheck

--- apps/services-func/src/functions/GetMessage/userPreferenceChecker/userPreferencesCheckerFactory.ts
+++ apps/services-func/src/functions/GetMessage/userPreferenceChecker/userPreferencesCheckerFactory.ts
@@ -93,7 +93,7 @@ export const userPreferenceCheckerVersionWithReadAuth: (
       TE.map(O.map((pref) => pref.accessReadMessageStatus)),
       // If user did not specify a custom authorization for the service,
       // we assume it's allowed to access read status
-      TE.map(O.getOrElse(() => AccessReadMessageStatusEnum.ALLOW)),
+      TE.map(O.getOrElseW(() => AccessReadMessageStatusEnum.ALLOW)),
       TE.map((readStatus) => readStatus !== AccessReadMessageStatusEnum.DENY),
     ),
 });
