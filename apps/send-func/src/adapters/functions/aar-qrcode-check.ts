46f39ed0ae2a6e2b65039ae3f86d3cd9950e040a
Luca Mancosu <luca.mancosu@it.ey.com>
2025-09-17 15:35:03 +0000

    add error handling

--- apps/send-func/src/adapters/functions/aar-qrcode-check.ts
+++ apps/send-func/src/adapters/functions/aar-qrcode-check.ts
@@ -1,11 +1,16 @@
 import {
   HttpHandler,
   HttpRequest,
-  HttpResponse,
+  HttpResponseInit,
   InvocationContext,
 } from "@azure/functions";
-import NotificationClient from "../send/notification.js";
-import { checkQrMandateRequestSchema } from "../send/definitions.js";
+import NotificationClient, {
+  NotificationClientError,
+} from "../send/notification.js";
+import {
+  checkQrMandateRequestSchema,
+  problemJsonSchema,
+} from "../send/definitions.js";
 
 export const aarQRCodeCheck =
   (
@@ -15,7 +20,7 @@ export const aarQRCodeCheck =
   async (
     request: HttpRequest,
     context: InvocationContext,
-  ): Promise<HttpResponse> => {
+  ): Promise<HttpResponseInit> => {
     try {
       const isTest = request.query.get("isTest") === "true";
 
@@ -23,16 +28,54 @@ export const aarQRCodeCheck =
       const parsedBody = checkQrMandateRequestSchema.parse(rawBody);
 
       const client = isTest ? uatNotificationClient : notificationClient;
-      await client.checkAarQrCodeIO(parsedBody, {});
-
-      return new HttpResponse({ status: 200 });
+      const response = await client.checkAarQrCodeIO(parsedBody, {});
+      return { status: 200, jsonBody: response };
     } catch (err) {
-      context.error("Errore in aarQRCodeCheck:", err);
-
       if (err instanceof Error && err.name === "ZodError") {
-        return new HttpResponse({ status: 400 });
+        context.error("aarQRCodeCheck malformed request:", err);
+        return {
+          status: 400,
+          jsonBody: {
+            type: "",
+            status: 400,
+            errors: [
+              {
+                code: "Malformed request",
+                detail: JSON.stringify(err.message),
+              },
+            ],
+          },
+        };
       }
 
-      return new HttpResponse({ status: 500 });
+      if (
+        err instanceof NotificationClientError &&
+        err.name === "NotificationClientError"
+      ) {
+        context.error("aarQRCodeCheck send error:", err);
+        return {
+          status: 500,
+          jsonBody: {
+            type: "",
+            status: err.status,
+            errors: [
+              {
+                code: "Internal server error",
+                detail: JSON.stringify(err.body),
+              },
+            ],
+          },
+        };
+      }
+
+      context.error("aarQRCodeCheck internal server error:", err);
+      return {
+        status: 500,
+        jsonBody: {
+          type: "",
+          status: 500,
+          errors: [{ code: "Internal server error" }],
+        },
+      };
     }
   };
