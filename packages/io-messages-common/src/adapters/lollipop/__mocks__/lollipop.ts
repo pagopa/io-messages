bb4f41e6889750ae4d5d6fe4c0bc5d885093ce07
acuiuli <andrea.cuiuli@gmail.com>
2025-09-18 10:02:02 +0200

    fix lollipop tests

--- packages/io-messages-common/src/adapters/lollipop/__mocks__/lollipop.ts
+++ packages/io-messages-common/src/adapters/lollipop/__mocks__/lollipop.ts
@@ -3,10 +3,12 @@ import { assertionRefSchema } from "../definitions/assertion-ref.js";
 import { assertionTypeSchema } from "../definitions/assertion-type.js";
 import { lcParamsSchema } from "../definitions/lc-params.js";
 import { problemJsonSchema } from "../definitions/problem-json.js";
-import { lollipopSignatureSchema } from "../definitions/signature.js";
+import { pubKeyStatusSchema } from "../definitions/pub-key-status.js";
+import { lollipopSignatureInputSchema } from "../definitions/signature-input.js";
 import { thumbprintSchema } from "../definitions/thumbprint.js";
 
-const aBearerToken = "aBearerTokenJWT";
+export const aBearerToken = "aBearerTokenJWT";
+export const aPubKey = "aPubKey";
 export const aFiscalCode = fiscalCodeSchema.parse("RMLGNN97R06F158N");
 
 export const anAssertionRef = assertionRefSchema.parse(
@@ -16,16 +18,21 @@ export const anAssertionRef = assertionRefSchema.parse(
 export const aThumbprint = thumbprintSchema.parse(
   "6LvipIvFuhyorHpUqK3HjySC5Y6gshXHFBhU9EJ4DoM=",
 );
-export const aSignatureInput = lollipopSignatureSchema.parse(
+export const aSignatureInput = lollipopSignatureInputSchema.parse(
   `sig1=("x-pagopa-lollipop-original-method" "x-pagopa-lollipop-original-url"); created=1618884475; keyid="${aThumbprint}"`,
 );
 
 export const anLcParams = lcParamsSchema.parse({
   assertion_file_name: `${aFiscalCode}-${anAssertionRef}`,
+  assertion_ref: anAssertionRef,
   assertion_type: assertionTypeSchema.Enum.OIDC,
   expired_at: new Date(),
   fiscal_code: aFiscalCode,
   lc_authentication_bearer: aBearerToken,
+  pub_key: aPubKey,
+  status: pubKeyStatusSchema.Enum.VALID,
+  ttl: 900,
+  version: 1,
 });
 
 export const aProblemJson = problemJsonSchema.parse({
