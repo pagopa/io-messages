ef6d6c9229de560221437f31d5b483dc434541bd
Roberto Cocco <robertococco@outlook.com>
2025-09-22 16:03:40 +0000

    Add test for USER_NOT_FOUND filter

--- apps/etl-func/src/domain/use-cases/__tests__/ingest-message-status.test.ts
+++ apps/etl-func/src/domain/use-cases/__tests__/ingest-message-status.test.ts
@@ -1,5 +1,5 @@
 import { aValidMessageStatus } from "@/__mocks__/message-status.js";
-import { describe, expect, test, vi } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 
 import { IngestMessageStatusUseCase } from "../ingest-message-status.js";
 
@@ -9,10 +9,42 @@ const ingestUseCase = new IngestMessageStatusUseCase({ publish: publishMock });
 const messageStatusBatch = [aValidMessageStatus];
 
 describe("ingestMessageStatusUseCase execute", () => {
+  beforeEach(() => {
+    publishMock.mockClear();
+  });
+
   test("Should call the publish method with 1 element", async () => {
     await expect(ingestUseCase.execute(messageStatusBatch)).resolves.toBe(
       undefined,
     );
+    expect(publishMock).toHaveBeenCalledTimes(1);
+    expect(publishMock).toHaveBeenCalledWith([
+      {
+        created_at: new Date(aValidMessageStatus.updatedAt).getTime(),
+        id: aValidMessageStatus.id,
+        is_archived: aValidMessageStatus.isArchived,
+        is_read: aValidMessageStatus.isRead,
+        message_id: aValidMessageStatus.messageId,
+        op: "CREATE",
+        schema_version: 1,
+        status: aValidMessageStatus.status,
+        version: aValidMessageStatus.version,
+      },
+    ]);
+  });
+
+  test("Should filter rejection reason USER_NOT_FOUND", async () => {
+    await expect(
+      ingestUseCase.execute([
+        aValidMessageStatus,
+        {
+          ...aValidMessageStatus,
+          id: "01FZ8Z7G6X6Z4XG5Y6Z7Z8Z9Z0",
+          status: "REJECTED",
+          rejection_reason: "USER_NOT_FOUND",
+        },
+      ]),
+    ).resolves.toBe(undefined);
     expect(publishMock).toHaveBeenCalledTimes(1);
     expect(publishMock).toHaveBeenCalledWith([
       {
