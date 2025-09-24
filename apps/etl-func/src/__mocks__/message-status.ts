92643b18228a358572313719382776625bdaaa16
EY Luca Mancosu <luca.mancosu@it.ey.com>
2025-09-24 11:49:51 +0200

    Change message-status-event schema
    Co-authored-by: Roberto Cocco <robertococco@outlook.com>
    Co-authored-by: garma <giovanniarmaleo00@gmail.com>

--- apps/etl-func/src/__mocks__/message-status.ts
+++ apps/etl-func/src/__mocks__/message-status.ts
@@ -12,3 +12,15 @@ export const aValidMessageStatus = messageStatusSchema.parse({
   updatedAt: "2019-11-25T15:03:12.684Z",
   version: 0,
 });
+
+export const aRejectedMessageStatus = messageStatusSchema.parse({
+  fiscalCode: aFiscalCode,
+  id: "01JD4YVX03H45HPGAB3E0Y2658-0000000000000000",
+  isArchived: false,
+  isRead: false,
+  messageId: "01JD4YVX03H45HPGAB3E0Y2658",
+  rejection_reason: "USER_NOT_FOUND",
+  status: "REJECTED",
+  updatedAt: "2019-11-25T15:03:12.684Z",
+  version: 0,
+});
