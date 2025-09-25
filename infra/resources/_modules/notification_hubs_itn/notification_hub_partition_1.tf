23063d1fbdfa35658fc82158af377be5448dc04a
garma <giovanniarmaleo00@gmail.com>
2025-09-25 17:11:28 +0200

    fix: split aert creation for notification hubs
    It is not possible to vreate a single alert for more notification hubs
    so we split those alerts.

--- infra/resources/_modules/notification_hubs_itn/notification_hub_partition_1.tf
+++ infra/resources/_modules/notification_hubs_itn/notification_hub_partition_1.tf
@@ -19,7 +19,7 @@ resource "azurerm_notification_hub" "partition_1" {
     bundle_id        = local.apns_credential.bundle_id
     team_id          = local.apns_credential.team_id
     key_id           = local.apns_credential.key_id
-    token = data.azurerm_key_vault_secret.ntfns_prod_token.value
+    token            = data.azurerm_key_vault_secret.ntfns_prod_token.value
   }
 
   gcm_credential {
@@ -53,4 +53,66 @@ resource "azurerm_notification_hub_authorization_rule" "partition_1_default_full
   manage                = true
   send                  = true
   listen                = true
+}
+
+resource "azurerm_monitor_metric_alert" "alert_nh_partition_1_pns_errors" {
+
+  name                = "[IOCOM|NH Partition 1] Push Notification Service errors"
+  resource_group_name = var.resource_group_name
+
+  scopes        = [azurerm_notification_hub.partition_1.id]
+  description   = "Notification Hub Partition 1 incurred in PNS errors, please check. Runbook: not needed."
+  severity      = 1
+  window_size   = "PT30M"
+  frequency     = "PT5M"
+  auto_mitigate = false
+
+  dynamic_criteria {
+    metric_namespace       = "Microsoft.NotificationHubs/namespaces/notificationHubs"
+    metric_name            = "outgoing.allpns.pnserror"
+    alert_sensitivity      = "Medium"
+    aggregation            = "Total"
+    operator               = "GreaterThan"
+    skip_metric_validation = false
+  }
+
+  # Action groups for alerts
+  action {
+    action_group_id = var.action_group_id
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_monitor_metric_alert" "alert_nh_partition_1_anomalous_pns_success_volume" {
+
+  name                = "[IOCOM|NH Partition 1] Push Notification Service anomalous success volume"
+  resource_group_name = var.resource_group_name
+
+  enabled = false
+
+  scopes        = [azurerm_notification_hub.partition_1.id]
+  description   = "Notification Hub Partition 1 has an anomalous PNS success volume. Runbook: not needed."
+  severity      = 1
+  window_size   = "PT5M"
+  frequency     = "PT1M"
+  auto_mitigate = false
+
+  dynamic_criteria {
+    metric_namespace         = "Microsoft.NotificationHubs/namespaces/notificationHubs"
+    metric_name              = "outgoing.allpns.success"
+    aggregation              = "Total"
+    operator                 = "GreaterThan"
+    alert_sensitivity        = "High"
+    evaluation_total_count   = 1
+    evaluation_failure_count = 1
+    skip_metric_validation   = false
+  }
+
+  # Action groups for alerts
+  action {
+    action_group_id = var.action_group_id
+  }
+
+  tags = var.tags
 }
