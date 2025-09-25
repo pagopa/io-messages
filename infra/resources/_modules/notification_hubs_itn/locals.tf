752aabb8d78505e225fcae7d1d9ab043618c57ff
garma <giovanniarmaleo00@gmail.com>
2025-09-25 11:38:45 +0200

    chore: remove unused local

--- infra/resources/_modules/notification_hubs_itn/locals.tf
+++ infra/resources/_modules/notification_hubs_itn/locals.tf
@@ -1,19 +1,5 @@
 locals {
 
-  nonstandard = {
-    weu = {
-      ntfns        = "${var.project}-ntfns-common-partition"
-      ntf          = "${var.project}-ntf-common-partition"
-      common_ntfns = "${var.project}-ntfns-common"
-      common_ntf   = "${var.project}-ntf-common"
-
-      sandbox_ntfns           = "${var.project}-ntfns-sandbox"
-      sandbox_partition_ntfns = "${var.project}-ntfns-sandbox-partition"
-      sandbox_ntf             = "${var.project}-ntf-sandbox"
-      sandbox_partition_ntf   = "${var.project}-ntf-sandbox-partition"
-    }
-  }
-
   apns_credential = {
     application_mode = "Production"
     bundle_id        = "it.pagopa.app.io"
