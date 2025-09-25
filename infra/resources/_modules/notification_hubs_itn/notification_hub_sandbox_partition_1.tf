eaebcbd49bde32d6c510fc9760336fba6f1eb82f
garma <giovanniarmaleo00@gmail.com>
2025-09-25 11:51:21 +0200

    perf: use free sku for sandbox nh

--- infra/resources/_modules/notification_hubs_itn/notification_hub_sandbox_partition_1.tf
+++ infra/resources/_modules/notification_hubs_itn/notification_hub_sandbox_partition_1.tf
@@ -3,7 +3,7 @@ resource "azurerm_notification_hub_namespace" "sandbox" {
   resource_group_name = var.resource_group_name
   location            = var.location
   namespace_type      = "NotificationHub"
-  sku_name            = "Standard"
+  sku_name            = "Free"
 
   tags = var.tags
 }
