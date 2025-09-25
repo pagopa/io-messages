df60dd8453a84661c930afe3d8a8c1a36c7d40d9
garma <giovanniarmaleo00@gmail.com>
2025-09-25 15:41:46 +0200

    use correct fragment

--- infra/resources/_modules/notification_hubs_itn/data.tf
+++ infra/resources/_modules/notification_hubs_itn/data.tf
@@ -1,19 +1,19 @@
 data "azurerm_key_vault_secret" "ntfns_prod_token" {
-  name         = "notification-hub-ntfns-common-ntf-common-token"
+  name         = "notification-hub-prod-token"
   key_vault_id = var.key_vault.id
 }
 
 data "azurerm_key_vault_secret" "ntfns_test_token" {
-  name         = "notification-hub-ntfns-common-ntf-common-token-sandbox"
+  name         = "notification-hub-dev-token"
   key_vault_id = var.key_vault.id
 }
 
 data "azurerm_key_vault_secret" "ntfns_prod_api_key" {
-  name         = "notification-hub-ntfns-common-ntf-common-api-key"
+  name         = "notification-hub-prod-api-key"
   key_vault_id = var.key_vault.id
 }
 
 data "azurerm_key_vault_secret" "ntfns_test_api_key" {
-  name         = "notification-hub-ntfns-common-ntf-common-api-key-sandbox"
+  name         = "notification-hub-dev-api-key"
   key_vault_id = var.key_vault.id
 }
