099dd93fb815a4e5ed2a3e84665fb79dbcb9ac1f
garma <giovanniarmaleo00@gmail.com>
2025-09-25 17:01:31 +0200

    chome: import nh resources to terraform state

--- infra/resources/prod/import.tf
+++ infra/resources/prod/import.tf
@@ -1,45 +1,49 @@
-
 import {
-  to = module.mongo.module.reminder_cosmos_account.azurerm_cosmosdb_account.this
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.DocumentDB/databaseAccounts/io-p-messages-reminder-mongodb-account"
+  to = module.notification_hubs_itn.azurerm_notification_hub_authorization_rule.partition_1_default_listen
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.NotificationHubs/namespaces/io-p-itn-com-nhns-01/notificationHubs/io-p-itn-com-nh-01/authorizationRules/DefaultListenSharedAccessSignature"
 }
 
 import {
-  to = module.mongo.azurerm_cosmosdb_mongo_database.db_reminder
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.DocumentDB/databaseAccounts/io-p-messages-reminder-mongodb-account/mongodbDatabases/db"
+  to = module.notification_hubs_itn.azurerm_notification_hub_authorization_rule.partition_1_default_full
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.NotificationHubs/namespaces/io-p-itn-com-nhns-01/notificationHubs/io-p-itn-com-nh-01/authorizationRules/DefaultFullSharedAccessSignature"
 }
 
 import {
-  to = module.mongo.module.mongdb_collection_reminder_sharded.azurerm_cosmosdb_mongo_collection.this
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.DocumentDB/databaseAccounts/io-p-messages-reminder-mongodb-account/mongodbDatabases/db/collections/reminder-sharded-new"
+  to = module.notification_hubs_itn.azurerm_notification_hub_authorization_rule.partition_2_default_listen
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.NotificationHubs/namespaces/io-p-itn-com-nhns-02/notificationHubs/io-p-itn-com-nh-02/authorizationRules/DefaultListenSharedAccessSignature"
 }
 
 import {
-  to = module.mongo.azurerm_key_vault_secret.mongodb_reminder_connection_string
-  id = "https://io-p-itn-com-kv-01.vault.azure.net/secrets/reminder-mongo-connection-string/f54d9211e926470fa3a19e241d665634"
+  to = module.notification_hubs_itn.azurerm_notification_hub_authorization_rule.partition_2_default_full
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.NotificationHubs/namespaces/io-p-itn-com-nhns-02/notificationHubs/io-p-itn-com-nh-02/authorizationRules/DefaultFullSharedAccessSignature"
 }
 
 import {
-  to = module.mongo.azurerm_key_vault_secret.mongodb_payments_connection_string
-  id = "https://io-p-itn-com-kv-01.vault.azure.net/secrets/paymentup-mongo-connection-string/6ff42f4d5f0a4dc5a2f0684c178f3537"
+  to = module.notification_hubs_itn.azurerm_notification_hub_authorization_rule.partition_3_default_listen
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.NotificationHubs/namespaces/io-p-itn-com-nhns-03/notificationHubs/io-p-itn-com-nh-03/authorizationRules/DefaultListenSharedAccessSignature"
 }
 
 import {
-  to = module.mongo.module.payments_cosmos_account.azurerm_cosmosdb_account.this
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.DocumentDB/databaseAccounts/io-p-payments-mongodb-account"
+  to = module.notification_hubs_itn.azurerm_notification_hub_authorization_rule.partition_3_default_full
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.NotificationHubs/namespaces/io-p-itn-com-nhns-03/notificationHubs/io-p-itn-com-nh-03/authorizationRules/DefaultFullSharedAccessSignature"
 }
 
 import {
-  to = module.mongo.azurerm_cosmosdb_mongo_database.db_payments
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.DocumentDB/databaseAccounts/io-p-payments-mongodb-account/mongodbDatabases/db"
+  to = module.notification_hubs_itn.azurerm_notification_hub_authorization_rule.partition_4_default_listen
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.NotificationHubs/namespaces/io-p-itn-com-nhns-04/notificationHubs/io-p-itn-com-nh-04/authorizationRules/DefaultListenSharedAccessSignature"
 }
 
 import {
-  to = module.mongo.module.mongdb_collection_payment_sharded.azurerm_cosmosdb_mongo_collection.this
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.DocumentDB/databaseAccounts/io-p-payments-mongodb-account/mongodbDatabases/db/collections/payment-sharded"
+  to = module.notification_hubs_itn.azurerm_notification_hub_authorization_rule.partition_4_default_full
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.NotificationHubs/namespaces/io-p-itn-com-nhns-04/notificationHubs/io-p-itn-com-nh-04/authorizationRules/DefaultFullSharedAccessSignature"
 }
 
 import {
-  to = module.mongo.module.mongdb_collection_payment_retry.azurerm_cosmosdb_mongo_collection.this
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.DocumentDB/databaseAccounts/io-p-payments-mongodb-account/mongodbDatabases/db/collections/payment-retry"
+  to = module.notification_hubs_itn.azurerm_notification_hub_authorization_rule.sandbox_default_listen
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.NotificationHubs/namespaces/io-p-itn-com-nhns-sandbox/notificationHubs/io-p-itn-com-nh-sandbox/authorizationRules/DefaultListenSharedAccessSignature"
+}
+
+import {
+  to = module.notification_hubs_itn.azurerm_notification_hub_authorization_rule.sandbox_default_full
+  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-com-rg-01/providers/Microsoft.NotificationHubs/namespaces/io-p-itn-com-nhns-sandbox/notificationHubs/io-p-itn-com-nh-sandbox/authorizationRules/DefaultFullSharedAccessSignature"
 }
