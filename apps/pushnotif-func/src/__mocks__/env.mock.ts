b20c047c989db84eda8c165ac524b8400044cf50
Roberto Cocco <robertococco@outlook.com>
2025-09-30 15:18:41 +0000

    Fix linting

--- apps/pushnotif-func/src/__mocks__/env.mock.ts
+++ apps/pushnotif-func/src/__mocks__/env.mock.ts
@@ -10,49 +10,49 @@ export const envConfig = {
   COSMOSDB_NAME: "CosmosDBName",
 
   COSMOSDB_URI: "CosmosDBUri",
-  MESSAGE_CONTAINER_NAME: "MessageContainerNane",
-  MESSAGE_CONTENT_STORAGE_CONNECTION_STRING:
-    "MessageContentStorageConnectionString",
-
-  NH_PARTITION_FEATURE_FLAG: "all",
-  NH1_ENDPOINT:
-    "Endpoint=sb://host.docker.internal:30000;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=foobar",
-  NH1_NAME: "NH1",
-
-  NH1_PARTITION_REGEX: "^[0-3]",
-  NH2_ENDPOINT:
-    "Endpoint=sb://host.docker.internal:30000;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=foobar",
-  NH2_NAME: "NH2",
-
-  NH2_PARTITION_REGEX: "^[4-7]",
-  NH3_ENDPOINT:
-    "Endpoint=sb://host.docker.internal:30000;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=foobar",
-  NH3_NAME: "NH3",
-  NH3_PARTITION_REGEX: "^[8-b]",
-  NH4_ENDPOINT:
-    "Endpoint=sb://host.docker.internal:30000;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=foobar",
-  NH4_NAME: "NH4",
-  NH4_PARTITION_REGEX: "^[c-f]",
-
   LEGACY_NH1_ENDPOINT:
     "Endpoint=sb://host.docker.internal:30000;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=foobar",
   LEGACY_NH1_NAME: "NH1",
-  LEGACY_NH1_PARTITION_REGEX: "^[0-3]",
 
+  LEGACY_NH1_PARTITION_REGEX: "^[0-3]",
   LEGACY_NH2_ENDPOINT:
     "Endpoint=sb://host.docker.internal:30000;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=foobar",
   LEGACY_NH2_NAME: "NH2",
-  LEGACY_NH2_PARTITION_REGEX: "^[4-7]",
 
+  LEGACY_NH2_PARTITION_REGEX: "^[4-7]",
   LEGACY_NH3_ENDPOINT:
     "Endpoint=sb://host.docker.internal:30000;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=foobar",
   LEGACY_NH3_NAME: "NH3",
-  LEGACY_NH3_PARTITION_REGEX: "^[8-b]",
 
+  LEGACY_NH3_PARTITION_REGEX: "^[8-b]",
   LEGACY_NH4_ENDPOINT:
     "Endpoint=sb://host.docker.internal:30000;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=foobar",
   LEGACY_NH4_NAME: "NH4",
   LEGACY_NH4_PARTITION_REGEX: "^[c-f]",
+  MESSAGE_CONTAINER_NAME: "MessageContainerNane",
+  MESSAGE_CONTENT_STORAGE_CONNECTION_STRING:
+    "MessageContentStorageConnectionString",
+  NH_PARTITION_FEATURE_FLAG: "all",
+
+  NH1_ENDPOINT:
+    "Endpoint=sb://host.docker.internal:30000;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=foobar",
+  NH1_NAME: "NH1",
+  NH1_PARTITION_REGEX: "^[0-3]",
+
+  NH2_ENDPOINT:
+    "Endpoint=sb://host.docker.internal:30000;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=foobar",
+  NH2_NAME: "NH2",
+  NH2_PARTITION_REGEX: "^[4-7]",
+
+  NH3_ENDPOINT:
+    "Endpoint=sb://host.docker.internal:30000;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=foobar",
+  NH3_NAME: "NH3",
+  NH3_PARTITION_REGEX: "^[8-b]",
+
+  NH4_ENDPOINT:
+    "Endpoint=sb://host.docker.internal:30000;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=foobar",
+  NH4_NAME: "NH4",
+  NH4_PARTITION_REGEX: "^[c-f]",
 
   NOTIFICATIONS_QUEUE_NAME: "notification-queue-name",
   NOTIFICATIONS_STORAGE_CONNECTION_STRING:
