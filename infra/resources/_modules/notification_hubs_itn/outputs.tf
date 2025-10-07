b523d6356df721bd86f5f5a9b39fcf605534fd1c
Roberto Cocco <robertococco@outlook.com>
2025-10-07 16:04:20 +0000

    Remove comment from notification hubs itn

--- infra/resources/_modules/notification_hubs_itn/outputs.tf
+++ infra/resources/_modules/notification_hubs_itn/outputs.tf
@@ -3,7 +3,6 @@ output "nh_itn_partition_1" {
     id       = azurerm_notification_hub.partition_1.id
     name     = azurerm_notification_hub.partition_1.name
     endpoint = azurerm_notification_hub_authorization_rule.partition_1_default_full.primary_connection_string
-    # endpoint = "https://${azurerm_notification_hub.partition_1.namespace_name}.servicebus.windows.net/${azurerm_notification_hub.partition_1.name}/"
   }
 }
 output "nh_itn_partition_2" {
