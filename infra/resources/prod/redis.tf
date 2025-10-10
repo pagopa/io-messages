9ecda861b65f4a719cdf3cad54a9cd1b49d00000
garma <64153597+Garma00@users.noreply.github.com>
2025-10-10 09:57:37 +0200

    Add redis alerts
    - Add an alert for high memory usage
    - Add an alert for high server load
    - Rename `com_new` to `com` redis resources
    
    Resolves IOCOM-2647

--- infra/resources/prod/redis.tf
+++ infra/resources/prod/redis.tf
@@ -1,4 +1,4 @@
-resource "azurerm_redis_cache" "com_new" {
+resource "azurerm_redis_cache" "com" {
 
   name = provider::dx::resource_name({
     prefix          = local.prefix
@@ -29,7 +29,7 @@ resource "azurerm_redis_cache" "com_new" {
   tags = local.tags
 }
 
-resource "azurerm_private_endpoint" "redis_cache_com_new" {
+resource "azurerm_private_endpoint" "redis_cache_com" {
 
   name = provider::dx::resource_name({
     prefix          = local.prefix
@@ -46,16 +46,69 @@ resource "azurerm_private_endpoint" "redis_cache_com_new" {
   subnet_id           = data.azurerm_subnet.pep.id
 
   private_dns_zone_group {
-    name                 = "${azurerm_redis_cache.com_new.name}-private-dns-zone-group"
+    name                 = "${azurerm_redis_cache.com.name}-private-dns-zone-group"
     private_dns_zone_ids = [data.azurerm_private_dns_zone.privatelink_redis_cache.id]
   }
 
   private_service_connection {
-    name                           = "${azurerm_redis_cache.com_new.name}-private-service-connection"
-    private_connection_resource_id = azurerm_redis_cache.com_new.id
+    name                           = "${azurerm_redis_cache.com.name}-private-service-connection"
+    private_connection_resource_id = azurerm_redis_cache.com.id
     is_manual_connection           = false
     subresource_names              = ["redisCache"]
   }
 
   tags = local.tags
+}
+
+resource "azurerm_monitor_metric_alert" "com_redis_memory_alert" {
+  name                = "${azurerm_redis_cache.com.name} high memory usage"
+  resource_group_name = azurerm_resource_group.itn_com.name
+  description         = "Alerts when Redis Cache memory usage exceeds 80%."
+  severity            = 1
+  enabled             = true
+
+  scopes = [
+    azurerm_redis_cache.com.id
+  ]
+
+  frequency   = "PT5M"
+  window_size = "PT5M"
+
+  criteria {
+    metric_namespace = "Microsoft.Cache/redis"
+    metric_name      = "Used Memory Percentage"
+    aggregation      = "Average"
+    operator         = "GreaterThanOrEqual"
+    threshold        = 80
+  }
+
+  action {
+    action_group_id = module.monitoring.action_group.id
+  }
+}
+resource "azurerm_monitor_metric_alert" "com_redis_server_load_alert" {
+  name                = "${azurerm_redis_cache.com.name} high server load"
+  resource_group_name = azurerm_resource_group.itn_com.name
+  description         = "Alerts when Redis Cache server load averages above 75% for 5 minutes."
+  severity            = 1
+  enabled             = true
+
+  scopes = [
+    azurerm_redis_cache.com.id
+  ]
+
+  frequency   = "PT5M"
+  window_size = "PT5M"
+
+  criteria {
+    metric_namespace = "Microsoft.Cache/redis"
+    metric_name      = "Server Load"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 75
+  }
+
+  action {
+    action_group_id = module.monitoring.action_group.id
+  }
 }
