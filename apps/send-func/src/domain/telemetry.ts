e8d451ada6d3b0d289091647ba0fcd002029ce5f
EY Luca Mancosu <luca.mancosu@it.ey.com>
2025-10-28 10:55:35 +0100

    Add send-func error telemetry for SEND AAR

---
+++ apps/send-func/src/domain/telemetry.ts
@@ -0,0 +1,11 @@
+export interface TelemetryService {
+  trackEvent(name: string, properties?: object): void;
+}
+
+export enum TelemetryEventName {
+  SEND_AAR_ATTACHMENT_SERVER_ERROR = "io.com.send.aar.attachment_server_error",
+  SEND_AAR_NOTIFICATION_SERVER_ERROR = "io.com.send.aar.notification_server_error",
+  SEND_AAR_QRCODE_CHECK_DATA_NOT_FOUND = "io.com.send.aar.qrcode_check_data_not_found",
+  SEND_AAR_QRCODE_CHECK_MALFORMED_403 = "io.com.send.aar.qrcode_check_malformed_403",
+  SEND_AAR_QRCODE_CHECK_SERVER_ERROR = "io.com.send.aar.qrcode_check_server_error",
+}
