9a8dc316b57645591406d9bb04f1eacfe41a84dc
EY Luca Mancosu <luca.mancosu@it.ey.com>
2025-11-21 14:26:18 +0100

    Add send-func autoscaler

--- infra/resources/_modules/web_apps/send_func.tf
+++ infra/resources/_modules/web_apps/send_func.tf
@@ -47,6 +47,50 @@ module "send_func" {
   action_group_id = var.action_group_id
 }
 
+module "send_func_autoscaler" {
+  source  = "pagopa-dx/azure-app-service-plan-autoscaler/azurerm"
+  version = "~> 1.0"
+
+  app_service_plan_id = module.send_func.function_app.plan.id
+  location            = var.environment.location
+
+  resource_group_name = module.send_func.function_app.resource_group_name
+
+  target_service = {
+    function_app = {
+      name = module.send_func.function_app.function_app.name
+    }
+  }
+
+  scale_metrics = {
+    cpu = {
+      cooldown_decrease         = 20,
+      cooldown_increase         = 3,
+      decrease_by               = 1,
+      increase_by               = 1,
+      lower_threshold           = 20,
+      statistic_decrease        = "Max",
+      statistic_increase        = "Max",
+      time_aggregation_decrease = "Maximum",
+      time_aggregation_increase = "Maximum",
+      time_window_decrease      = 5,
+      time_window_increase      = 2,
+      upper_threshold           = 70
+    },
+  }
+
+  scheduler = {
+    normal_load = {
+      default = 2,
+      minimum = 2
+    },
+
+    maximum = 30,
+  }
+
+  tags = var.tags
+}
+
 resource "azurerm_role_assignment" "key_vault_send_func_secrets_user" {
   scope                = var.key_vault.id
   role_definition_name = "Key Vault Secrets User"
