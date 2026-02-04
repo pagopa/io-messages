4548bcc25712002786a1f87852d8fee924971025
Gianmaria Scorza <gianmaria@scorza.dev>
2026-02-04 10:11:25 +0100

    move namedvalue to dedicated file

--- infra/resources/_modules/apim/backend_api.tf
+++ infra/resources/_modules/apim/backend_api.tf
@@ -5,16 +5,6 @@ resource "azurerm_api_management_api_version_set" "communications_v1" {
   display_name        = "Communications AppBackend v1"
   versioning_scheme   = "Segment"
 }
-
-resource "azurerm_api_management_named_value" "app_backend_key" {
-  name                = "io-communications-app-backend-key"
-  api_management_name = data.azurerm_api_management.apim_itn_platform_api.name
-  resource_group_name = data.azurerm_api_management.apim_itn_platform_api.resource_group_name
-  display_name        = "io-communications-app-backend-key"
-  value               = data.azurerm_key_vault_secret.app_backend_api_key_secret.value
-  secret              = true
-}
-
 resource "azurerm_api_management_api" "communications" {
   name                  = "io-p-communications-api"
   api_management_name   = data.azurerm_api_management.apim_itn_platform_api.name
