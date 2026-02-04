93bedb7f0a2efdbc6f375a201c7349000e552f8b
Gianmaria Scorza <gianmaria@scorza.dev>
2026-02-04 10:09:00 +0100

    move namedvalue to dedicated file

--- infra/resources/_modules/apim/apim_namedvalues.tf
+++ infra/resources/_modules/apim/apim_namedvalues.tf
@@ -29,3 +29,12 @@ resource "azurerm_api_management_named_value" "io-p-itn-com-pushnotif-func-key" 
   value               = data.azurerm_key_vault_secret.pushnotif_func_key.value
   secret              = "true"
 }
+
+resource "azurerm_api_management_named_value" "app_backend_key" {
+  name                = "io-communications-app-backend-key"
+  api_management_name = data.azurerm_api_management.apim_itn_platform_api.name
+  resource_group_name = data.azurerm_api_management.apim_itn_platform_api.resource_group_name
+  display_name        = "io-communications-app-backend-key"
+  value               = data.azurerm_key_vault_secret.app_backend_api_key_secret.value
+  secret              = true
+}
