ef41cc581417e8e61cb32c7c2379d627b67f75fc
Luca Mancosu <luca.mancosu@it.ey.com>
2026-02-13 11:58:45 +0100

    change autoscaler

--- infra/resources/_modules/web_apps/citizen-func.tf
+++ infra/resources/_modules/web_apps/citizen-func.tf
@@ -168,23 +168,23 @@ module "citizen_func_autoscaler_new" {
 
   scheduler = {
     normal_load = {
-      default = 16,
-      minimum = 16
+      default = 11,
+      minimum = 6
     },
 
-    # low_load = {
-    #   minimum = 2,
-    #   name    = "low_load_profile",
-    #   default = 10,
-    #   start = {
-    #     hour    = 22,
-    #     minutes = 0
-    #   }
-    #   end = {
-    #     hour    = 5,
-    #     minutes = 0
-    #   },
-    # },
+    low_load = {
+      minimum = 2,
+      name    = "low_load_profile",
+      default = 10,
+      start = {
+        hour    = 22,
+        minutes = 0
+      }
+      end = {
+        hour    = 5,
+        minutes = 0
+      },
+    },
 
     maximum = 30,
   }
