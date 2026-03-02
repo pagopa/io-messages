be2436b349373a7c52f08909d08d1c8f3e173905
Luca Mancosu <luca.mancosu@it.ey.com>
2026-03-02 15:39:55 +0100

    add isTracingDisabled

--- apps/services-func/src/utils/appinsights.ts
+++ apps/services-func/src/utils/appinsights.ts
@@ -2,6 +2,7 @@ import { initAppInsights } from "@pagopa/ts-commons/lib/appinsights";
 import * as ai from "applicationinsights";
 
 import { IConfig } from "./config";
+import { env } from "process";
 
 // Avoid to initialize Application Insights more than once
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
@@ -9,5 +10,8 @@ export const initTelemetryClient = (config: IConfig): ai.TelemetryClient =>
   ai.defaultClient
     ? ai.defaultClient
     : initAppInsights(config.APPLICATIONINSIGHTS_CONNECTION_STRING, {
+        // We need to disable tracing only when we are testing locally because
+        // interfere with azurite docker container.
+        isTracingDisabled: !env.isProduction,
         samplingPercentage: 5,
       });
