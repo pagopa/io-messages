94e75b0b8716e41b9b83468441d2d01e8ca0bcd6
Mirco Civolani <mirco.civolani@pagopa.it>
2026-03-26 13:35:21 +0100

    add io-ecosystem-n-links to codeowners
    add io-ecosystem-n-links to codeowners

--- infra/repository/locals.tf
+++ infra/repository/locals.tf
@@ -3,7 +3,7 @@ locals {
     name                     = "io-messages"
     description              = "This is a monorepo that contains all the backend microservices and functionalities related to messaging in IO."
     topics                   = ["backend", "io", "messages", "comunicazione", "iocom"]
-    reviewers_teams          = ["io-communication-backend", "engineering-team-cloud-eng"]
+    reviewers_teams          = ["io-communication-backend", "engineering-team-cloud-eng", "io-ecosystem-n-links"]
     default_branch_name      = "main"
     infra_cd_policy_branches = ["main"]
     opex_cd_policy_branches  = ["main"]
