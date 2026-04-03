42771f74e166436f46bb148bbecfc16d5f25c613
garma <giovanniarmaleo00@gmail.com>
2026-04-03 17:02:41 +0200

    Add massive_progress container infrastructure
    Run precommit

--- infra/resources/_modules/web_apps/pushnotif-func.tf
+++ infra/resources/_modules/web_apps/pushnotif-func.tf
@@ -74,7 +74,8 @@ locals {
       NH4_NAME            = var.nh_itn_partition_4.name
       # ------------------------------------------------------------------------------
 
-      MASSIVE_JOBS_CONTAINER_NAME = "massive-jobs"
+      MASSIVE_JOBS_CONTAINER_NAME     = "massive-jobs"
+      MASSIVE_PROGRESS_CONTAINER_NAME = "massive-progress",
 
       MESSAGE_CONTAINER_NAME                    = "message-content"
       MESSAGE_CONTENT_STORAGE_CONNECTION_STRING = var.message_content_storage.connection_string
