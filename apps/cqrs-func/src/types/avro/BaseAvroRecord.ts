e1a54758b8a711c2547089d2a63852a9da20596c
Gianmaria Scorza <gianmaria@scorza.dev>
2026-04-09 18:39:16 +0200

    Port BaseAvroRecord Abstract Class from Lib

--- apps/cqrs-func/src/types/avro/BaseAvroRecord.ts
+++ apps/cqrs-func/src/types/avro/BaseAvroRecord.ts
@@ -1,1 +1,8 @@
-export { BaseAvroRecord } from "@chasdevs/avro-to-typescript";
+export abstract class BaseAvroRecord {
+  public static readonly schema = {};
+  public static readonly subject: string = "";
+
+  public abstract schema(): object;
+
+  public abstract subject(): string;
+}
