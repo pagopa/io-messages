54b30da81f2d212a7dddafca0dbcd2675a4bb917
Gianmaria Scorza <gianmaria@scorza.dev>
2026-04-28 08:23:29 +0000

    add vitest.config.mjs config file across all apps

--- apps/pushnotif-func/vitest.config.mjs
+++ apps/pushnotif-func/vitest.config.mjs
@@ -1,12 +1,6 @@
-import * as path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   test: {
     coverage: {
       exclude: [
