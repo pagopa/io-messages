caac4c901846aeb13e3cf9b5d45b5358c8f48acd
Christian Calabrese <christian.calabrese@pagopa.it>
2026-05-11 16:55:59 +0200

    Remove moved blocks after apply

--- infra/bootstrapper/prod/moved.tf
+++
@@ -1,9 +0,0 @@
-moved {
-  from = module.repo.azurerm_key_vault_access_policy.infra_ci_kv_common["/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.KeyVault/vaults/io-p-kv-common"]
-  to   = azurerm_key_vault_access_policy.infra_ci_kv_common["/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.KeyVault/vaults/io-p-kv-common"]
-}
-
-moved {
-  from = module.repo.azurerm_key_vault_access_policy.infra_cd_kv_common["/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.KeyVault/vaults/io-p-kv-common"]
-  to   = azurerm_key_vault_access_policy.infra_cd_kv_common["/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-common/providers/Microsoft.KeyVault/vaults/io-p-kv-common"]
-}
