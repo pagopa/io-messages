061b892a661b5c7f93897cb163d9cdfd34f73814
Luca Mancosu <luca.mancosu@it.ey.com>
2026-07-07 16:47:57 +0200

    remove data usage for key vault secret values retrieve

--- infra/resources/_modules/apim/apim_namedvalues.tf
+++ infra/resources/_modules/apim/apim_namedvalues.tf
@@ -1,14 +1,9 @@
-data "azurerm_key_vault_secret" "rc_func_key" {
-  name         = "rc-func-key"
-  key_vault_id = local.key_vault.id
-}
-
 resource "azurerm_api_management_named_value" "io_p_itn_com_rc_func_key" {
   name                = "io-p-itn-com-rc-func-key"
   display_name        = "io-p-itn-com-rc-func-key"
   api_management_name = data.azurerm_api_management.apim_itn_api.name
   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-  value               = data.azurerm_key_vault_secret.rc_func_key.value
+  value               = "@Microsoft.KeyVault(VaultName=${local.key_vault.name};SecretName=rc-func-key)"
   secret              = "true"
 }
 
@@ -17,7 +12,7 @@ resource "azurerm_api_management_named_value" "io_p_itn_com_services_func_key" {
   display_name        = "io-p-itn-com-services-func-key"
   api_management_name = data.azurerm_api_management.apim_itn_api.name
   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-  value               = data.azurerm_key_vault_secret.services_func_key.value
+  value               = "@Microsoft.KeyVault(VaultName=${local.key_vault.name};SecretName=services-func-key)"
   secret              = "true"
 }
 
@@ -26,7 +21,7 @@ resource "azurerm_api_management_named_value" "io-p-itn-com-pushnotif-func-key" 
   display_name        = "io-p-itn-com-pushnotif-func-key"
   api_management_name = data.azurerm_api_management.apim_itn_api.name
   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-  value               = data.azurerm_key_vault_secret.pushnotif_func_key.value
+  value               = "@Microsoft.KeyVault(VaultName=${local.key_vault.name};SecretName=pushnotif-func-key)"
   secret              = "true"
 }
 
@@ -35,6 +30,6 @@ resource "azurerm_api_management_named_value" "app_backend_key" {
   api_management_name = data.azurerm_api_management.apim_itn_platform_api.name
   resource_group_name = data.azurerm_api_management.apim_itn_platform_api.resource_group_name
   display_name        = "io-communications-app-backend-key"
-  value               = data.azurerm_key_vault_secret.app_backend_api_key_secret.value
+  value               = "@Microsoft.KeyVault(VaultName=${var.common_key_vault.name};SecretName=appbackend-APP-BACKEND-PRIMARY-KEY)"
   secret              = true
 }
