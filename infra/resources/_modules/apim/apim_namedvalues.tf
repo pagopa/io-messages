ea5558185bac62583f3d154f33a8fe57d62a8592
Luca Mancosu <luca.mancosu@it.ey.com>
2026-07-13 14:42:23 +0200

    retrieve secret values  with value from key vault

--- infra/resources/_modules/apim/apim_namedvalues.tf
+++ infra/resources/_modules/apim/apim_namedvalues.tf
@@ -3,8 +3,10 @@ resource "azurerm_api_management_named_value" "io_p_itn_com_rc_func_key" {
   display_name        = "io-p-itn-com-rc-func-key"
   api_management_name = data.azurerm_api_management.apim_itn_api.name
   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-  value               = "@Microsoft.KeyVault(VaultName=${local.key_vault.name};SecretName=rc-func-key)"
-  secret              = "true"
+  value_from_key_vault {
+    secret_id = "${local.key_vault.vault_uri}secrets/rc-func-key"
+  }
+  secret = "true"
 }
 
 resource "azurerm_api_management_named_value" "io_p_itn_com_services_func_key" {
@@ -12,8 +14,10 @@ resource "azurerm_api_management_named_value" "io_p_itn_com_services_func_key" {
   display_name        = "io-p-itn-com-services-func-key"
   api_management_name = data.azurerm_api_management.apim_itn_api.name
   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-  value               = "@Microsoft.KeyVault(VaultName=${local.key_vault.name};SecretName=services-func-key)"
-  secret              = "true"
+  value_from_key_vault {
+    secret_id = "${local.key_vault.vault_uri}secrets/services-func-key"
+  }
+  secret = "true"
 }
 
 resource "azurerm_api_management_named_value" "io-p-itn-com-pushnotif-func-key" {
@@ -21,8 +25,10 @@ resource "azurerm_api_management_named_value" "io-p-itn-com-pushnotif-func-key" 
   display_name        = "io-p-itn-com-pushnotif-func-key"
   api_management_name = data.azurerm_api_management.apim_itn_api.name
   resource_group_name = data.azurerm_api_management.apim_itn_api.resource_group_name
-  value               = "@Microsoft.KeyVault(VaultName=${local.key_vault.name};SecretName=pushnotif-func-key)"
-  secret              = "true"
+  value_from_key_vault {
+    secret_id = "${local.key_vault.vault_uri}secrets/pushnotif-func-key"
+  }
+  secret = "true"
 }
 
 resource "azurerm_api_management_named_value" "app_backend_key" {
@@ -30,6 +36,9 @@ resource "azurerm_api_management_named_value" "app_backend_key" {
   api_management_name = data.azurerm_api_management.apim_itn_platform_api.name
   resource_group_name = data.azurerm_api_management.apim_itn_platform_api.resource_group_name
   display_name        = "io-communications-app-backend-key"
-  value               = "@Microsoft.KeyVault(VaultName=${var.common_key_vault.name};SecretName=appbackend-APP-BACKEND-PRIMARY-KEY)"
-  secret              = true
+  value_from_key_vault {
+    secret_id = "${var.common_key_vault.vault_uri}secrets/appbackend-APP-BACKEND-PRIMARY-KEY"
+  }
+
+  secret = true
 }
