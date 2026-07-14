65f3cff321bea5b932c120f249583dab752b2dff
garma <64153597+Garma00@users.noreply.github.com>
2026-07-14 15:11:06 +0200

    Add kv secret role to apim

--- infra/resources/_modules/apim/rbac.tf
+++ infra/resources/_modules/apim/rbac.tf
@@ -3,3 +3,9 @@ resource "azurerm_role_assignment" "remote_content_slot_key_vault_secrets_user" 
   role_definition_name = "Key Vault Secrets User"
   principal_id         = data.azurerm_api_management.apim_itn_api.identity[0].principal_id
 }
+
+resource "azurerm_role_assignment" "apim_platform_key_vault_secrets_user" {
+  scope                = var.key_vault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = data.azurerm_api_management.apim_itn_platform_api.identity[0].principal_id
+}
