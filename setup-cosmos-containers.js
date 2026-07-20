a837c56de5b6d8c2b94364ffbd89cc1e85488df4
Roberto Cocco <robertococco@outlook.com>
2026-07-20 10:22:32 +0200

    Add services and profiles

--- setup-cosmos-containers.js
+++ setup-cosmos-containers.js
@@ -25,6 +25,140 @@ const containers = [
   { id: "notifications", partitionKey: "/messageId" },
 ];
 
+const profiles = [
+  {
+    containerId: "profiles",
+    document: {
+      fiscalCode: "LVTEST00A00A200X",
+      isEmailEnabled: true,
+      isEmailValidated: true,
+      isInboxEnabled: true,
+      isTestProfile: false,
+      isWebhookEnabled: true,
+      email: "lvtest@example.com",
+      servicePreferencesSettings: {
+        mode: "AUTO",
+        version: 1,
+      },
+      lastAppVersion: "UNKNOWN",
+      pushNotificationsContentType: "UNSET",
+      reminderStatus: "ENABLED",
+      id: "LVTEST00A00A200X-0000000000000000",
+      version: 0,
+      kind: "INewProfile",
+    },
+  },
+  {
+    containerId: "profiles",
+    document: {
+      fiscalCode: "LVTEST00A00A199X",
+      isEmailEnabled: true,
+      isEmailValidated: true,
+      isInboxEnabled: true,
+      isTestProfile: false,
+      isWebhookEnabled: true,
+      email: "lvtest@example.com",
+      servicePreferencesSettings: {
+        mode: "AUTO",
+        version: 1,
+      },
+      lastAppVersion: "UNKNOWN",
+      pushNotificationsContentType: "UNSET",
+      reminderStatus: "ENABLED",
+      id: "LVTEST00A00A199X-0000000000000000",
+      version: 0,
+      kind: "INewProfile",
+    },
+  },
+  {
+    containerId: "profiles",
+    document: {
+      fiscalCode: "LVTEST00A00A198X",
+      isEmailEnabled: true,
+      isEmailValidated: true,
+      isInboxEnabled: true,
+      isTestProfile: false,
+      isWebhookEnabled: true,
+      email: "lvtest@example.com",
+      servicePreferencesSettings: {
+        mode: "AUTO",
+        version: 1,
+      },
+      lastAppVersion: "UNKNOWN",
+      pushNotificationsContentType: "UNSET",
+      reminderStatus: "ENABLED",
+      id: "LVTEST00A00A198X-0000000000000000",
+      version: 0,
+      kind: "INewProfile",
+    },
+  },
+  {
+    containerId: "profiles",
+    document: {
+      fiscalCode: "LVTEST00A00A197X",
+      isEmailEnabled: true,
+      isEmailValidated: true,
+      isInboxEnabled: true,
+      isTestProfile: false,
+      isWebhookEnabled: true,
+      email: "lvtest@example.com",
+      servicePreferencesSettings: {
+        mode: "AUTO",
+        version: 1,
+      },
+      lastAppVersion: "UNKNOWN",
+      pushNotificationsContentType: "UNSET",
+      reminderStatus: "ENABLED",
+      id: "LVTEST00A00A197X-0000000000000000",
+      version: 0,
+      kind: "INewProfile",
+    },
+  },
+  {
+    containerId: "profiles",
+    document: {
+      fiscalCode: "LVTEST00A00A196X",
+      isEmailEnabled: true,
+      isEmailValidated: true,
+      isInboxEnabled: true,
+      isTestProfile: false,
+      isWebhookEnabled: true,
+      email: "lvtest@example.com",
+      servicePreferencesSettings: {
+        mode: "AUTO",
+        version: 1,
+      },
+      lastAppVersion: "UNKNOWN",
+      pushNotificationsContentType: "UNSET",
+      reminderStatus: "ENABLED",
+      id: "LVTEST00A00A196X-0000000000000000",
+      version: 0,
+      kind: "INewProfile",
+    },
+  },
+];
+
+const services = [
+  {
+    containerId: "services",
+    document: {
+      id: "01JR0QRJ8MX1PD06DE6X5FWXS5-0000000000000000",
+      serviceId: "01JR0QRJ8MX1PD06DE6X5FWXS5",
+      serviceName: "harness",
+      authorizedRecipients: [],
+      authorizedCIDRs: [],
+      departmentName: "District Practical Rubber Bacon",
+      isVisible: true,
+      maxAllowedPaymentAmount: 8635,
+      organizationFiscalCode: "38291048166",
+      organizationName: "Wilkinson LLC",
+      requireSecureChannels: false,
+      version: 0,
+      kind: "INewService",
+    },
+  },
+];
+
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const waitForCosmos = async () => {
@@ -59,6 +193,18 @@ const createContainerIfNotExists = async (database, { id, partitionKey }) => {
   }
 };
 
+const addDocumentToContainer = async (database, containerId, document) => {
+  try {
+    await database.container(containerId).items.upsert(document, {
+      disableAutomaticIdGeneration: true,
+    });
+    console.log(`Document ${document.id} ready in container ${containerId}.`);
+  } catch (error) {
+    console.error(`Error adding document to container ${containerId}:`, error);
+    throw error;
+  }
+};
+
 (async () => {
   await waitForCosmos();
 
@@ -70,6 +216,18 @@ const createContainerIfNotExists = async (database, { id, partitionKey }) => {
   await Promise.all(
     containers.map((container) =>
       createContainerIfNotExists(database, container),
+    ),
+  );
+
+  await Promise.all(
+    profiles.map(({ containerId, document }) =>
+      addDocumentToContainer(database, containerId, document),
+    ),
+  );
+
+  await Promise.all(
+    services.map(({ containerId, document }) =>
+      addDocumentToContainer(database, containerId, document),
     ),
   );
 })();
