321c99b1be63e9fedc4302296c2690ad2ed643a8
garma <giovanniarmaleo00@gmail.com>
2026-08-27 11:35:47 +0200

    Add MessageCreatedEventPublisher port

---
+++ apps/messages-app/src/application/ports/message-created-event.ts
@@ -0,0 +1,24 @@
+import { GenericError } from "@pagopa/hexagonal-core";
+import { messageIDSchema } from "io-messages-common/domain/message";
+import { Result } from "neverthrow";
+import z from "zod";
+
+const defaultAddressesSchema = z.object({
+  email: z.email().optional(),
+});
+
+export const messageCreatedEventSchema = z.object({
+  defaultAddresses: defaultAddressesSchema.optional(),
+  messageId: messageIDSchema,
+  serviceVersion: z.number().nonnegative(),
+});
+export type MessageCreatedEvent = z.TypeOf<typeof messageCreatedEventSchema>;
+
+export interface MessageCreatedEventPublisher {
+  /**
+   * Publishes the event notifying that a message has been created.
+   *
+   * Returns a `GenericError` when the event cannot be published.
+   */
+  publish: (event: MessageCreatedEvent) => Promise<Result<void, GenericError>>;
+}
