c0ae2549932f1ca8c4d870f74aaa7ee3bdaf1d80
Luca Mancosu <luca.mancosu@it.ey.com>
2026-09-16 11:22:18 +0200

    disable etl rc message conf changefeed

--- infra/resources/_modules/web_apps/remote-content-func.tf
+++ infra/resources/_modules/web_apps/remote-content-func.tf
@@ -32,8 +32,6 @@ locals {
       FETCH_KEEPALIVE_MAX_FREE_SOCKETS    = "10"
       FETCH_KEEPALIVE_FREE_SOCKET_TIMEOUT = "30000"
       FETCH_KEEPALIVE_TIMEOUT             = "60000"
-
-      "AzureWebJobs.CosmosRemoteContentMessageConfigurationChangeFeed.Disabled" = "1"
     }
   }
 }
