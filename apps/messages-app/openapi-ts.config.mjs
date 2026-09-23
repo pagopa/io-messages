11083c12eb37dafbf3bf43099be3c0cc9d69681e
garma <giovanniarmaleo00@gmail.com>
2026-09-23 14:52:11 +0200

    Add third-party api spec with hey-api code generation
    Fix rc typo

--- apps/messages-app/openapi-ts.config.mjs
+++ apps/messages-app/openapi-ts.config.mjs
@@ -1,15 +1,30 @@
 /** @type {import("@hey-api/openapi-ts").UserConfig} */
-export default {
-  input:
-    "https://raw.githubusercontent.com/pagopa/io-backend/d75e74049bc2f2268881e052ecfd92391305bcbd/openapi/consumed/pagopa_ecommerce.yaml",
-  output: {
-    importFileExtension: ".js",
-    path: "src/generated/pagopa-ecommerce",
+export default [
+  {
+    input:
+      "https://raw.githubusercontent.com/pagopa/io-backend/d75e74049bc2f2268881e052ecfd92391305bcbd/openapi/consumed/pagopa_ecommerce.yaml",
+    output: {
+      importFileExtension: ".js",
+      path: "src/generated/pagopa-ecommerce",
+    },
+    plugins: [
+      "@hey-api/client-fetch",
+      "@hey-api/typescript",
+      "@hey-api/sdk",
+      "zod",
+    ],
   },
-  plugins: [
-    "@hey-api/client-fetch",
-    "@hey-api/typescript",
-    "@hey-api/sdk",
-    "zod",
-  ],
-};
+  {
+    input: "./api/consumed/api-third-party.yaml",
+    output: {
+      importFileExtension: ".js",
+      path: "src/generated/remote-content",
+    },
+    plugins: [
+      "@hey-api/client-fetch",
+      "@hey-api/typescript",
+      "@hey-api/sdk",
+      "zod",
+    ],
+  },
+];
